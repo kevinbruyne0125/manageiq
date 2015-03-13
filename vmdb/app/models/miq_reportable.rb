kevinbruyne0125/manageiq

commit 936fbaf5e107a13fc31deace3bcfcfdd719ed452

remove one read from aar_columns
looks like this code set aar_columns to an empty array, then just passed
the empty array along.  Lets just pass an empty array along

vmdb/app/models/miq_reportable.rb
@@ -3,9 +3,6 @@ module MiqReportable
   def self.records2table(records, options)
     return Ruport::Data::Table.new if records.blank?
 
-    db = records.first.class
-    db.aar_columns = []
-
     data = records.map {|r|
       options[:include]["categories"] = options[:include_categories] if options[:include] && options[:include_categories]
       r.reportable_data(:include => options[:include],
@@ -17,7 +14,7 @@ module MiqReportable
 
     data = data[0..options[:limit] - 1] if options[:limit] # apply limit after includes are processed
     Ruport::Data::Table.new(:data => data,
-                            :column_names => db.aar_columns,
+                            :column_names => [],
                             :record_class => options[:record_class],
                             :filters => options[:filters])
   end

commit a34803eff53c05cae8a2ce794c64890ece2516fa

Fixed quadicon issue when editing multiple hosts
The commit 936fbaf5e107a13fc31deace3bcfcfdd719ed452 broke the Infrastructure/Hosts multiple items editor.
https://bugzilla.redhat.com/show_bug.cgi?id=1201092

vmdb/app/models/miq_reportable.rb
@@ -3,6 +3,9 @@ module MiqReportable
   def self.records2table(records, options)
     return Ruport::Data::Table.new if records.blank?
 
+    db = records.first.class
+    db.aar_columns = []
+
     data = records.map {|r|
       options[:include]["categories"] = options[:include_categories] if options[:include] && options[:include_categories]
       r.reportable_data(:include => options[:include],
@@ -14,7 +17,7 @@ module MiqReportable
 
     data = data[0..options[:limit] - 1] if options[:limit] # apply limit after includes are processed
     Ruport::Data::Table.new(:data => data,
-                            :column_names => [],
+                            :column_names => db.aar_columns,
                             :record_class => options[:record_class],
                             :filters => options[:filters])
   end