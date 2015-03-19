kevinbruyne0125/manageiq

commit 80fbeeceecf0701ebebdabc6acd484df86af64ea

Gettext: Convert simple translation keys to gettext form
Simple keys == keys without variables

vmdb/app/mailers/generic_mailer.rb
@@ -127,7 +127,7 @@ class GenericMailer < ActionMailer::Base
     options = {
       :to      => to,
       :from    => settings[:from],
-      :subject => "#{I18n.t("product.name")} Test Email",
+      :subject => "#{_("ManageIQ")} Test Email",
       :body    => "If you have received this email, your SMTP settings are correct."
     }
     prepare_generic_email(options)

commit bac18463cd588dc6d364f071258bc20ca87eef42

Change gettext back to I18n for test email subject.
This was broken in commit: 80fbeeceecf0701ebe

https://bugzilla.redhat.com/show_bug.cgi?id=1202681

vmdb/app/mailers/generic_mailer.rb
@@ -127,7 +127,7 @@ class GenericMailer < ActionMailer::Base
     options = {
       :to      => to,
       :from    => settings[:from],
-      :subject => "#{_("ManageIQ")} Test Email",
+      :subject => "#{I18n.t("product.name")} Test Email",
       :body    => "If you have received this email, your SMTP settings are correct."
     }
     prepare_generic_email(options)

vmdb/spec/mailers/generic_mailer_spec.rb
@@ -201,7 +201,8 @@ describe GenericMailer do
 
   describe "#test_mail" do
     it "should be called directly" do
-      GenericMailer.test_email(@args[:to], settings = {})
+      mail = GenericMailer.test_email(@args[:to], settings = {})
+      mail.subject.should start_with I18n.t("product.name")
     end
 
     it "should not change the input parameters" do