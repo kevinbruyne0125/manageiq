kevinbruyne0125/manageiq

commit ec1389e716c91aa5857984165b5653fb97c3a5bc

Fixed issue where pressing form button on sub-list view was blowing up.
- Fixed to not show toolbars when pressing Manage Policies button, there shouldn't be toolbar buttons on forms.
- Fixed code to build @ view when user pressed save/cancel buttons on forms such as Editing Tags or Managing Policies from sub-list view screens. Moved show_association, show_details methods to a common module and removed individual methods from other controllers.

https://bugzilla.redhat.com/show_bug.cgi?id=1193847

vmdb/app/controllers/application_controller/ci_processing.rb
@@ -508,16 +508,12 @@ module ApplicationController::CiProcessing
 
     # Build the vm detail gtl view
   def show_details(db, options={})  # Pass in the db, parent vm is in @vm
-    dbname = db.to_s.downcase
-    association = options[:association] || nil
-
+    association    = options[:association] || nil
     # generate the grid/tile/list url to come back here when gtl buttons are pressed
-    @gtl_url = "/#{@db}/" + @listicon.pluralize + "/" + @record.id.to_s + "?"
-
-    @showtype = "details"
-
+    @gtl_url       = "/#{@db}/#{@listicon.pluralize}/#{@record.id.to_s}?"
+    @showtype      = "details"
     @no_checkboxes = true
-    @showlinks = true
+    @showlinks     = true
 
     @view, @pages = get_view(db,
                             :parent=>@record,
@@ -1851,4 +1847,45 @@ module ApplicationController::CiProcessing
     @edit[:new][owner] != @edit[:current][owner]
   end
 
+  def show_association(action, display_name, listicon, method, klass, association = nil)
+    @explorer = true if request.xml_http_request? # Ajax request means in explorer
+    if @explorer  # Save vars for tree history array
+      @x_show = params[:x_show]
+      @sb[:action] = @lastaction = action
+    end
+    @record = identify_record(params[:id])
+    @view = session[:view]                  # Restore the view from the session to get column names for the display
+    return if record_no_longer_exists?(@record, klass.to_s)
+    @lastaction = action
+    if params[:show] || params[:x_show]
+      id = params[:show] ? params[:show] : params[:x_show]
+      if method.kind_of?(Array)
+        obj = @record
+        while meth = method.shift do
+          obj = obj.send(meth)
+        end
+        @item = obj.find(from_cid(id))
+      else
+        @item = @record.send(method).find(from_cid(id))
+      end
+
+      drop_breadcrumb({:name => "#{@record.name} (#{display_name})",
+                       :url => "/#{controller_name}/#{action}/#{@record.id}?page=#{@current_page}"})
+      drop_breadcrumb(:name => @item.name,
+                      :url => "/#{controller_name}/#{action}/#{@record.id}?show=#{@item.id}")
+      @view = get_db_view(klass, :association=>association)
+      show_item
+    else
+      drop_breadcrumb({:name => @record.name,
+                       :url  => "/#{controller_name}/show/#{@record.id}"}, true)
+      drop_breadcrumb(:name => "#{@record.name} (#{display_name})",
+                      :url  => "/#{controller_name}/#{action}/#{@record.id}")
+      @listicon = listicon
+      if association.nil?
+        show_details(klass)
+      else
+        show_details(klass, :association => association )
+      end
+    end
+  end
 end

vmdb/app/controllers/application_controller/policy_support.rb
@@ -210,6 +210,7 @@ module ApplicationController::PolicySupport
     end
     @edit[:current] = @edit[:new].dup                 # Save the existing counts
     session[:changed] = false
+    @in_a_form = true
     protect_build_tree                                # Build the protect tree
     build_targets_hash(@politems)
   end

vmdb/app/controllers/host_controller.rb
@@ -140,39 +140,6 @@ class HostController < ApplicationController
     end
   end
 
-  def show_association(action, display_name, listicon, method, klass, association = nil)
-    @host = @record = identify_record(params[:id])
-    @view = session[:view]                  # Restore the view from the session to get column names for the display
-    return if record_no_longer_exists?(@host, 'Host')
-
-    @lastaction = action
-    unless params[:show].nil?
-      if method.kind_of?(Array)
-        obj = @host
-        while meth = method.shift do
-          obj = obj.send(meth)
-        end
-        @item = obj.find(from_cid(params[:show]))
-      else
-        @item = @host.send(method).find(from_cid(params[:show]))
-      end
-
-      drop_breadcrumb( { :name => "#{@host.name} (#{display_name})", :url=>"/host/#{action}/#{@host.id}?page=#{@current_page}"} )
-      drop_breadcrumb( { :name => @item.name,                        :url=>"/host/#{action}/#{@host.id}?show=#{@item.id}"} )
-      show_item
-    else
-      drop_breadcrumb( { :name => @host.name,                        :url=>"/host/show/#{@host.id}"}, true )
-      drop_breadcrumb( { :name => "#{@host.name} (#{display_name})", :url=>"/host/#{action}/#{@host.id}"} )
-      @listicon = listicon
-      if association.nil?
-        show_details(klass)
-      else
-        show_details(klass, :association => association )
-      end
-    end
-
-  end
-
   def filesystems
     show_association('filesystems', 'Files', 'filesystems', :filesystems, Filesystem)
   end
@@ -191,35 +158,6 @@ class HostController < ApplicationController
 
   def guest_applications
     show_association('guest_applications', 'Packages', 'guest_application', :guest_applications, GuestApplication)
-  end
-
-    # Build the vm detail gtl view
-  def show_details(db, options={})  # Pass in the db, parent vm is in @vm
-    dbname = db.to_s.downcase
-    association = options[:association] || nil
-
-    # generate the grid/tile/list url to come back here when gtl buttons are pressed
-    @gtl_url = "/host/" + @lastaction + "/" + @host.id.to_s + "?"
-
-    @showtype = "details"
-    @no_checkboxes = true
-    @showlinks = true
-
-    @view, @pages = get_view(db,
-                            :parent=>@host,
-                            :association=>association,
-                            :dbname=>"hostitem")  # Get the records into a view & paginator
-
-
-    # Came in from outside, use RJS to redraw gtl partial
-    if params[:ppsetting]  || params[:searchtag] || params[:entry] || params[:sort_choice]
-#     render :update do |page|                    # Use RJS to update the display
-#       page.replace_html("main_div", :partial=>"layouts/gtl", :locals=>{:action_url=>@lastaction})
-#     end
-      replace_gtl_main_div
-    else
-      render :action => 'show'
-    end
   end
 
   def toggle_policy_profile

vmdb/app/controllers/orchestration_stack_controller.rb
@@ -176,66 +176,6 @@ class OrchestrationStackController < ApplicationController
 
   private ############################
 
-  def show_association(action, display_name, listicon, method, klass, association = nil)
-    @orchestration_stack = @record = identify_record(params[:id])
-    @view = session[:view]                  # Restore the view from the session to get column names for the display
-    return if record_no_longer_exists?(@orchestration_stack, 'OrchestrationStack')
-
-    @lastaction = action
-    if params[:show]
-      if method.kind_of?(Array)
-        obj = @orchestration_stack
-        while meth = method.shift do
-          obj = obj.send(meth)
-        end
-        @item = obj.find(from_cid(params[:show]))
-      else
-        @item = @orchestration_stack.send(method).find(from_cid(params[:show]))
-      end
-
-      drop_breadcrumb(:name => "#{@orchestration_stack.name} (#{display_name})",
-                      :url  => "/orchestration_stack/#{action}/#{@orchestration_stack.id}?page=#{@current_page}")
-      drop_breadcrumb(:name => @item.respond_to?("name") ? @item.name : @item.key,
-                      :url  => "/orchestration_stack/#{action}/#{@orchestration_stack.id}?show=#{@item.id}")
-      show_item
-    else
-      drop_breadcrumb({:name => @orchestration_stack.name,
-                       :url  => "/orchestration_stack/show/#{@orchestration_stack.id}"}, true)
-      drop_breadcrumb(:name => "#{@orchestration_stack.name} (#{display_name})",
-                      :url  => "/orchestration_stack/#{action}/#{@orchestration_stack.id}")
-      @listicon = listicon
-      if association.nil?
-        show_details(klass)
-      else
-        show_details(klass, :association => association)
-      end
-    end
-  end
-
-  def show_details(db, options = {})  # Pass in the db, parent vm is in @vm
-    dbname = db.to_s.downcase
-    association = options[:association] || nil
-
-    # generate the grid/tile/list url to come back here when gtl buttons are pressed
-    @gtl_url = "/orchestration_stack/" + @lastaction + "/" + @orchestration_stack.id.to_s + "?"
-
-    @showtype = "details"
-    @no_checkboxes = true
-    @showlinks = true
-
-    @view, @pages = get_view(db,
-                             :parent      => @orchestration_stack,
-                             :association => association,
-                             :dbname      => "orchestrationstackitem")  # Get the records into a view & paginator
-
-    # Came in from outside, use RJS to redraw gtl partial
-    if params[:ppsetting]  || params[:searchtag] || params[:entry] || params[:sort_choice]
-      replace_gtl_main_div
-    else
-      render :action => 'show'
-    end
-  end
-
   def get_session_data
     @title      = "Stack"
     @layout     = "orchestration_stack"

vmdb/app/controllers/storage_controller.rb
@@ -196,39 +196,6 @@ class StorageController < ApplicationController
     end
   end
 
-  def show_association(action, display_name, listicon, method, klass, association = nil)
-    @storage = @record = identify_record(params[:id])
-    @view = session[:view]                  # Restore the view from the session to get column names for the display
-    return if record_no_longer_exists?(@storage)
-
-    @lastaction = action
-    unless params[:show].nil?
-      if method.kind_of?(Array)
-        obj = @storage
-        while meth = method.shift do
-          obj = obj.send(meth)
-        end
-        @item = obj.find(params[:show])
-      else
-        @item = @storage.send(method).find(from_cid(params[:show]))
-      end
-
-      drop_breadcrumb( { :name => "#{@storage.name} (#{display_name})", :url=>"/storage/#{action}/#{@storage.id}?page=#{@current_page}"} )
-      drop_breadcrumb( { :name => @item.name,                           :url=>"/storage/#{action}/#{@storage.id}?show=#{@item.id}"} )
-      show_item
-    else
-      drop_breadcrumb( { :name => "#{@storage.name} (#{display_name})", :url=>"/storage/#{action}/#{@storage.id}"} )
-      @listicon = listicon
-      @showtype = "details"
-      if association.nil?
-        show_details(klass)
-      else
-        show_details(klass, :association => association )
-      end
-    end
-
-  end
-
   def files
     show_association('files', 'All Files', 'storage_files', :storage_files, StorageFile, 'files')
   end
@@ -251,31 +218,6 @@ class StorageController < ApplicationController
 
   def debris_files
     show_association('debris_files', 'Non-VM Files', 'storage_non_vm_files', :storage_files, StorageFile, 'debris_files')
-  end
-
-  # Build the vm detail gtl view
-  def show_details(db, options={})  # Pass in the db, parent storage is in @storage
-    association = options[:association] || nil
-    condition = options[:conditions] || nil
-
-    # generate the grid/tile/list url to come back here when gtl buttons are pressed
-    @gtl_url = "/storage/" + @lastaction + "/" + @storage.id.to_s + "?"
-
-    @showtype = "details"
-    @no_checkboxes = true
-    @showlinks = true
-
-    @view, @pages = get_view(db,
-                            :parent => @storage,
-                            :association => association,
-                            :conditions => condition) # Get the records into a view & paginator
-
-    # Came in from outside, use RJS to redraw gtl partial
-    if params[:ppsetting]  || params[:searchtag] || params[:entry] || params[:sort_choice]
-      replace_gtl_main_div
-    else
-      render :action => 'show'
-    end
   end
 
   private ############################

vmdb/app/controllers/vm_common.rb
@@ -613,46 +613,6 @@ module VmCommon
     new_column.text = "Percent Used of Provisioned Size"
   end
 
-  def show_association(action, display_name, listicon, method, klass, association = nil)
-    @explorer = true if request.xml_http_request? # Ajax request means in explorer
-    if @explorer  # Save vars for tree history array
-      @action = action
-      @x_show = params[:x_show]
-    end
-    @vm = @record = identify_record(params[:id], VmOrTemplate)
-    return if record_no_longer_exists?(@vm)
-    rec_cls = "vm"
-
-    @sb[:action] = @lastaction = action
-    if params[:show] != nil || params[:x_show] != nil
-      id = params[:show] ? params[:show] : params[:x_show]
-      if method.kind_of?(Array)
-        obj = @record
-        while meth = method.shift do
-          obj = obj.send(meth)
-        end
-        @item = obj.find(from_cid(id))
-      else
-        @item = @record.send(method).find(from_cid(id))
-      end
-
-      drop_breadcrumb( { :name => "#{@record.name} (#{display_name})", :url=>"/#{rec_cls}/#{action}/#{@record.id}?page=#{@current_page}"} )
-      drop_breadcrumb( { :name => @item.name,                      :url=>"/#{rec_cls}/#{action}/#{@record.id}?show=#{@item.id}"} )
-      @view = get_db_view(klass, :association=>association)
-      show_item
-    else
-      drop_breadcrumb( { :name => @record.name,                        :url=>"/#{rec_cls}/show/#{@record.id}"}, true )
-      drop_breadcrumb( { :name => "#{@record.name} (#{display_name})", :url=>"/#{rec_cls}/#{action}/#{@record.id}"} )
-      @listicon = listicon
-      if association.nil?
-        show_details(klass)
-      else
-        show_details(klass, :association => association )
-      end
-    end
-
-  end
-
   def processes
     show_association('processes', 'Running Processes', 'processes', [:operating_system, :processes], OsProcess, 'processes')
   end

vmdb/spec/controllers/application_controller/ci_processing_spec.rb
@@ -190,3 +190,46 @@ describe ApplicationController do
     end
   end
 end
+
+describe HostController do
+  context "#show_association" do
+    before(:each) do
+      @host = FactoryGirl.create(:host)
+      @guest_application = FactoryGirl.create(:guest_application, :name => "foo", :host_id => @host.id)
+    end
+    it "renders show_item" do
+      controller.instance_variable_set(:@_params, :id => @host.id, :show => @guest_application.id)
+      controller.instance_variable_set(:@breadcrumbs, [])
+      controller.stub(:get_view)
+      controller.should_receive(:show_item).once
+      controller.send(:show_association,
+                      'guest_applications',
+                      'Packages',
+                      'guest_application',
+                      :guest_applications,
+                      GuestApplication)
+      expect(response.status).to eq(200)
+      expect(assigns(:breadcrumbs)).to eq([{:name => "#{@host.name} (Packages)",
+                                            :url  => "/host/guest_applications/#{@host.id}?page="},
+                                           {:name => "foo",
+                                            :url  => "/host/guest_applications/#{@host.id}?show=#{@guest_application.id}"}
+                                          ])
+    end
+
+    it "renders show_details" do
+      controller.instance_variable_set(:@_params, :id => @host.id)
+      controller.instance_variable_set(:@breadcrumbs, [])
+      controller.stub(:get_view)
+      controller.should_receive(:show_details).once
+      controller.send(:show_association,
+                      'guest_applications',
+                      'Packages',
+                      'guest_application',
+                      :guest_applications,
+                      GuestApplication)
+      expect(response.status).to eq(200)
+      expect(assigns(:breadcrumbs)).to eq([{:name => "#{@host.name} (Packages)",
+                                            :url  => "/host/guest_applications/#{@host.id}"}])
+    end
+  end
+end

commit 23ebdd1624b47eca36b3acc0a1c2c5410c74d9e0

Make show_association use controller_to_model when retrieving @record
This makes linux_initprocesses (and others) load from VmOrTemplate,
not VmCloud - without it, /vm_cloud/linux_initprocesses/119 tries to
load VmCloud(id=119) and not VmOrTemplate(id=119) in CiProcessing#show_association.

This has been broken since ec1389e which unified all the
show_association methods, without the special @record load handling
that was present in VmCommon. Without it, a load from VmCloud is
attempted instead of VmOrTemplate, and fails.

This likely affects *all* show_association calls made from vm_common -
Running Processes, Registry Entries, Advanced Settings, Init Processes,
Win32 Services, Kernel Drivers, File System Drivers, Files and Security Groups.

https://bugzilla.redhat.com/show_bug.cgi?id=1211665

vmdb/app/controllers/application_controller/ci_processing.rb
@@ -1897,14 +1897,14 @@ module ApplicationController::CiProcessing
     @edit[:new][owner] != @edit[:current][owner]
   end
 
-  def show_association(action, display_name, listicon, method, klass, association = nil, conditions =nil)
+  def show_association(action, display_name, listicon, method, klass, association = nil, conditions = nil)
     # Ajax request means in explorer, or if current explorer is one of the explorer controllers
     @explorer = true if request.xml_http_request? && explorer_controller?
     if @explorer  # Save vars for tree history array
       @x_show = params[:x_show]
       @sb[:action] = @lastaction = action
     end
-    @record = identify_record(params[:id])
+    @record = identify_record(params[:id], controller_to_model)
     @view = session[:view]                  # Restore the view from the session to get column names for the display
     return if record_no_longer_exists?(@record, klass.to_s)
     @lastaction = action