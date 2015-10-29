kevinbruyne0125/manageiq

commit 3483c632cb9a590920f1c731d1697bd39b88c90b

Fixed dropdonws in Add new Group https://bugzilla.redhat.com/show_bug.cgi?id=1265188

app/controllers/ops_controller/ops_rbac.rb
@@ -778,6 +778,11 @@ module OpsController::OpsRbac
     end
 
     changed = (@edit[:new] != @edit[:current])
+    bad = false
+    if rec_type == "group"
+      bad ||= @edit[:new][:role].blank?
+      bad ||= @edit[:new][:group_tenant].blank?
+    end
 
     render :update do |page|              # Use JS to update the display
       if %w(up down).include?(params[:button])
@@ -798,7 +803,7 @@ module OpsController::OpsRbac
           page << set_element_visible('group_lookup', @edit[:new][:lookup]) unless params[:check]
         end
       end
-      page << javascript_for_miq_button_visibility(changed)
+      page << javascript_for_miq_button_visibility(changed && !bad)
     end
   end
 
@@ -1037,10 +1042,11 @@ module OpsController::OpsRbac
 
     all_roles = MiqUserRole.all
     @edit[:roles] = {}
-    @edit[:roles]["<Choose a role>"] = nil
+    @edit[:roles]["<Choose a Role>"] = nil if @record.id.nil?
     all_roles.each do |r|
       @edit[:roles][r.name] = r.id
     end
+
     if @group.miq_user_role.nil?              # If adding, set to first role
       @edit[:new][:role] = @edit[:roles][@edit[:roles].keys.sort[0]]
     else
@@ -1050,6 +1056,7 @@ module OpsController::OpsRbac
     @edit[:projects_tenants] = []
     all_tenants = Tenant.all_tenants
     all_projects = Tenant.all_projects
+    @edit[:projects_tenants].push(["", [["<Choose a Project/Tentant>", :selected => "<Choose a Project/Tentant>", :disabled => "<Choose a Project/Tentant>", :style => 'display:none']]])
     @edit[:projects_tenants].push(["Projects", all_projects.sort_by(&:name).collect { |tenant| [tenant.name, tenant.id] }]) unless all_projects.blank?
     @edit[:projects_tenants].push(["Tenants", all_tenants.sort_by(&:name).collect { |tenant| [tenant.name, tenant.id] }]) unless all_tenants.blank?
     @edit[:new][:group_tenant] = @group.tenant_id

app/views/ops/_rbac_group_details.html.haml
@@ -48,7 +48,6 @@
         - else
           = select_tag('group_role',
               options_for_select(@edit[:roles].sort, @edit[:new][:role]),
-              "title" => "Choose",
               :class  => "selectpicker")
           :javascript
             miqInitSelectPicker();
@@ -74,10 +73,10 @@
         - else
           = select_tag('group_tenant',
               grouped_options_for_select(@edit[:projects_tenants], @edit[:new][:group_tenant]),
-              "title" => "Choose",
-              :class  => "selectpicker")
+               :class  => "selectpicker")
           :javascript
             miqSelectPickerEvent("group_tenant", "#{combo_url}")
+
     - unless @edit
       .form-group
         %label.col-md-2.control-label

commit f66f070fb4165e7c2dc5f16462a68099cd16bbef

Hide default tenant groups in the list for LDAP sequence ordering. Fix the save button for the group sequence edit screen broken by commit 3483c63

app/controllers/ops_controller/ops_rbac.rb
@@ -441,7 +441,7 @@ module OpsController::OpsRbac
     @edit = {}
     @edit[:new] = {}
     @edit[:current] = {}
-    @edit[:new][:ldap_groups] = MiqGroup.all(:order => "sequence ASC")  # Get the groups from the DB
+    @edit[:new][:ldap_groups] = MiqGroup.non_tenant_groups.sort_by(&:sequence)  #Get the non-tenant groups from the DB
     @edit[:new][:ldap_groups_list] = []
     @edit[:new][:ldap_groups].each do |g|
       @edit[:new][:ldap_groups_list].push(g.description)
@@ -810,6 +810,7 @@ module OpsController::OpsRbac
       if %w(up down).include?(params[:button])
         page.replace("flash_msg_div", :partial => "layouts/flash_msg") unless @refresh_div && @refresh_div != "column_lists"
         page.replace(@refresh_div, :partial => @refresh_partial) if @refresh_div
+        bad = false
       else
         # only do following for groups
         if x_node.split("-").first == "g" || x_node == "xx-g"
@@ -821,7 +822,7 @@ module OpsController::OpsRbac
           page << "$('#description').val('#{j_str(@edit[:new][:description])}');" if params[:ldap_groups_user]
           page << javascript_for_tree_checkbox_clicked(tree_name) if params[:check] && tree_name
 
-          # don't do anythingto lookup box when checkboxes on the right side are checked
+          # don't do anything to lookup box when checkboxes on the right side are checked
           page << set_element_visible('group_lookup', @edit[:new][:lookup]) unless params[:check]
         end
       end