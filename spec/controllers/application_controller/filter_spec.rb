kevinbruyne0125/manageiq

commit 9fe2859189465eef376793ed91cb176cd70379ce

consistently access current_user.settings

app/controllers/application_controller/filter.rb
@@ -711,9 +711,9 @@ module ApplicationController::Filter
         if @settings[:default_search] && @settings[:default_search][@edit[@expkey][:exp_model].to_s.to_sym] # See if a default search exists
           def_search = @settings[:default_search][@edit[@expkey][:exp_model].to_s.to_sym]
           if id.to_i == def_search.to_i
-            db_user = current_user
-            db_user.settings[:default_search].delete(@edit[@expkey][:exp_model].to_s.to_sym)
-            db_user.save
+            user_settings = current_user.settings || {}
+            user_settings[:default_search].delete(@edit[@expkey][:exp_model].to_s.to_sym)
+            current_user.update_attributes(:settings => user_settings)
             @edit[:adv_search_applied] = nil          # clearing up applied search results
           end
         end
@@ -916,16 +916,15 @@ module ApplicationController::Filter
         end
       end
       if @flash_array.blank?
-        db_user = current_user
-        if db_user != nil
-          db_user.settings[:default_search] ||= Hash.new                        # Create the col widths hash, if not there
-          db_user.settings[:default_search][cols_key] ||= Hash.new        # Create hash for the view db
-          @settings[:default_search] ||= Hash.new                               # Create the col widths hash, if not there
-          @settings[:default_search][cols_key] ||= Hash.new             # Create hash for the view db
-          @settings[:default_search][cols_key] = params[:id].to_i # Save each cols width
-          db_user.settings[:default_search][cols_key] = @settings[:default_search][cols_key]
-          db_user.save
-        end
+        @settings[:default_search] ||= {}
+        @settings[:default_search][cols_key] ||= {}
+        @settings[:default_search][cols_key] = params[:id].to_i
+
+        user_settings = current_user.settings || {}
+        user_settings[:default_search] ||= {}
+        user_settings[:default_search][cols_key] ||= {}
+        user_settings[:default_search][cols_key] = @settings[:default_search][cols_key]
+        user.update_attributes(:settings => user_settings)
       end
     end
     build_listnav_search_list(@view.db) if @flash_array.blank?

app/controllers/application_controller/explorer.rb
@@ -37,13 +37,13 @@ module ApplicationController::Explorer
 
     if params.key?(:width)
       # Store the new settings in the user record and in @settings (session)
-      db_user = current_user
-      unless db_user.nil?
-        db_user.settings[:explorer] ||= {}
-        db_user.settings[:explorer][params[:controller]] ||= {}
-        db_user.settings[:explorer][params[:controller]][:width] = params['width']
-        @settings[:explorer] = db_user.settings[:explorer]
-        db_user.save
+      if current_user
+        user_settings = current_user.settings || {}
+        user_settings[:explorer] ||= {}
+        user_settings[:explorer][params[:controller]] ||= {}
+        user_settings[:explorer][params[:controller]][:width] = params['width']
+        @settings[:explorer] = user_settings[:explorer]
+        current_user.update_attributes(:settings => user_settings)
       end
     end
 

commit 60246f3af8b293763ff8a002077b2f380df70158

fix setting default filter
broke in 9fe2859189465eef376793ed91cb176cd70379ce

reported in https://bugzilla.redhat.com/show_bug.cgi?id=1276788

app/controllers/application_controller/filter.rb
@@ -924,7 +924,7 @@ module ApplicationController::Filter
         user_settings[:default_search] ||= {}
         user_settings[:default_search][cols_key] ||= {}
         user_settings[:default_search][cols_key] = @settings[:default_search][cols_key]
-        user.update_attributes(:settings => user_settings)
+        current_user.update_attributes(:settings => user_settings)
       end
     end
     build_listnav_search_list(@view.db) if @flash_array.blank?

spec/controllers/application_controller/filter_spec.rb
@@ -45,4 +45,22 @@ describe ApplicationController do
       session[:edit][:expression][:expression].should == edit[:new][:expression]
     end
   end
+
+  describe "#save_default_search" do
+    let(:user) { FactoryGirl.create(:user_with_group, :settings => {:default_search => {}}) }
+    it "saves settings" do
+      search = FactoryGirl.create(:miq_search, :name => 'sds')
+
+      login_as user
+      controller.instance_variable_set(:@settings, {})
+      controller.instance_variable_set(:@_response, double.as_null_object)
+      controller.instance_variable_set(:@_params, :id => search.id)
+      session[:view] = controller.send(:get_db_view, Host)
+
+      controller.send(:save_default_search)
+      user.reload
+
+      expect(user.settings).to eq(:default_search => {:Host => search.id})
+    end
+  end
 end