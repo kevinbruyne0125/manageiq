kevinbruyne0125/manageiq

commit fa9984f845b08e20a85064acea8a3bdf033534cf

Code and migration for handling serialized MiqReport objects in binary blobs as hashes. Fixed up migration for converting Miqreport objs in MiqReportResults.

vmdb/app/models/miq_report_result.rb
@@ -61,23 +61,11 @@ class MiqReportResult < ActiveRecord::Base
   end
 
   def report_results
-    if self.report.kind_of?(String)
-      # support legacy reports that saved results in the report column
-      return nil if self.report.nil?
-      Marshal.load(Base64.decode64(self.report.split("\n").join))
-    elsif self.binary_blob
+    if self.binary_blob
       serializer_name = self.binary_blob.data_type
       serializer_name = "Marshal" unless serializer_name == "YAML"  # YAML or Marshal, for now
       serializer = serializer_name.constantize
-      raw = self.binary_blob.binary.sub(/MiqReport/, 'Hash')
-      # MiqReport.from_hash(serializer.load(raw))
-      h=serializer.load(raw)
-
-      h.merge!('attributes' => h.delete('attributes').to_hash)
-      h.delete('new_record')
-      h.delete('raw_attributes')
-
-      MiqReport.from_hash(h)
+      MiqReport.from_hash(serializer.load(self.binary_blob.binary))
     elsif self.report.kind_of?(MiqReport)
       return self.report
     else
@@ -87,7 +75,7 @@ class MiqReportResult < ActiveRecord::Base
 
   def report_results=(value)
     self.binary_blob = BinaryBlob.new(:name => "report_results", :data_type => "YAML")
-    self.binary_blob.binary = YAML.dump(value)
+    self.binary_blob.binary = YAML.dump(value.to_hash)
   end
 
   def report_html=(html)

vmdb/app/models/miq_report.rb
@@ -196,7 +196,7 @@ class MiqReport < ActiveRecord::Base
 
   def to_hash
     keys = self.class.attr_accessor_that_yamls
-    keys.each_with_object(self.attributes.deep_clone) {|k, h| h[k] = self.send(k)}
+    keys.each_with_object(self.attributes.to_hash) {|k, h| h[k] = self.send(k)}
   end
 
   def self.from_hash(h)

vmdb/db/migrate/20150619153304_miq_report_result_fix_serialized_report.rb
@@ -2,6 +2,9 @@ class MiqReportResultFixSerializedReport < ActiveRecord::Migration
   class MiqReportResult < ActiveRecord::Base
     self.inheritance_column = :_type_disabled
 
+    YAML_ATTRS = [:table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name, :extras, :record_id,
+                  :tl_times, :user_categories, :trend_data, :performance, :include_for_find, :report_run_time, :chart, :reserved]
+
     def serialize_report_to_hash
       val = read_attribute(:report)
 
@@ -10,8 +13,20 @@ class MiqReportResultFixSerializedReport < ActiveRecord::Migration
       else
         raise "unexpected format of report attribute encountered,  '#{val.inspect}'"
       end
+      raw_hash = YAML.load(val).last
+      new_hash = YAML_ATTRS.each_with_object(raw_hash['attributes'].to_hash) { |k, h| h[k.to_s] = raw_hash[k.to_s] }
 
-      YAML.dump(YAML.load(val).last['attributes'])
+      YAML.dump(new_hash)
+    end
+
+    def serialize_hash_to_report
+      val = read_attribute(:report)
+
+      if val.starts_with?("---")
+        YAML.dump([MiqReport.new(YAML.load(val))])
+      else
+        raise "unexpected format of report attribute encountered,  '#{val.inspect}'"
+      end
     end
   end
 
@@ -25,6 +40,9 @@ class MiqReportResultFixSerializedReport < ActiveRecord::Migration
 
   def down
     say_with_time("Converting MiqReportResult#report back to a serialized MiqReport") do
+      MiqReportResult.where('report IS NOT NULL').each do |rr|
+        rr.update_attribute(:report, rr.serialize_hash_to_report)
+      end
     end
   end
 end

vmdb/db/migrate/20150623222927_binary_blob_fix_serialized_report.rb
@@ -1,0 +1,86 @@
+class BinaryBlobFixSerializedReport < ActiveRecord::Migration
+  class BinaryBlob < ActiveRecord::Base
+    self.inheritance_column = :_type_disabled
+    has_many :binary_blob_parts, -> { order(:id) }
+
+    YAML_ATTRS = [:table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name, :extras, :record_id,
+                  :tl_times, :user_categories, :trend_data, :performance, :include_for_find, :report_run_time, :chart, :reserved]
+
+    def serialize_report_to_hash
+      val = binary
+
+      if val.include?("!ruby/object:MiqReport")
+        val.sub!(/MiqReport/, 'Hash')
+      else
+        raise "unexpected format of binary data encountered,  '#{val.inspect}'"
+      end
+
+      raw_hash = YAML.load(val)
+      new_hash = YAML_ATTRS.each_with_object(raw_hash['attributes'].to_hash) { |k, h| h[k.to_s] = raw_hash[k.to_s] }
+
+      YAML.dump(new_hash)
+    end
+
+    def serialize_hash_to_report
+      val = binary
+
+      if val.starts_with?("---")
+        YAML.dump([MiqReport.new(YAML.load(val))])
+      else
+        raise "unexpected format of report attribute encountered,  '#{val.inspect}'"
+      end
+    end
+
+    def delete_binary
+      self.md5 = self.size = self.part_size = nil
+      self.binary_blob_parts.delete_all
+      self.save!
+    end
+
+    def binary
+      # TODO: Change this to collect the binary_blob_parts in batches, so we are not pulling in every row into memory at once
+      data = self.binary_blob_parts.inject("") { |d, b| d << b.data; d }
+      raise "size of #{self.class.name} id [#{self.id}] is incorrect" unless self.size.nil? || self.size == data.bytesize
+      raise "md5 of #{self.class.name} id [#{self.id}] is incorrect" unless self.md5.nil? || self.md5 == Digest::MD5.hexdigest(data)
+      return data
+    end
+
+    def binary=(data)
+      data.force_encoding('ASCII-8BIT')
+      self.delete_binary unless self.parts == 0
+      return self if data.bytesize == 0
+
+      self.part_size ||= BinaryBlobPart.default_part_size
+      self.md5 = Digest::MD5.hexdigest(data)
+      self.size = data.bytesize
+
+      until data.bytesize == 0
+        buf = data.slice!(0..self.part_size)
+        self.binary_blob_parts << BinaryBlobPart.new(:data => buf)
+      end
+      self.save!
+
+      return self
+    end
+
+    def parts
+      self.binary_blob_parts.size
+    end
+  end
+
+  def up
+    say_with_time("Converting BinaryBlob report results to a serialized hash") do
+      BinaryBlob.where(:resource_type=>'MiqReportResult').each do |bb|
+        bb.binary = bb.serialize_report_to_hash
+      end
+    end
+  end
+
+  def down
+    say_with_time("Converting BinaryBlob report results back to a serialized MiqReport") do
+      BinaryBlob.where(:resource_type=>'MiqReportResult').each do |bb|
+        bb.binary = bb.serialize_report_to_hash
+      end
+    end
+  end
+end

commit a2c58fe7245552838feb04018a99b2f982ad270b

Fix report result serialization and deserialization
It looks like a bug was introduced in fa9984f845b0 where
`MiqReportResult.report_results=` was only expecting a `MiqReport`
object, whereas `#_async_generate_result` will send it a CSV, plain
text, or PDF. It calls `#to_hash` on the object which results in a `NoMethodError`.

As a quick fix, this commit switches on type on both the setter and
getter so that the object passed in can be serialized and deserialized correctly.

Fixes https://bugzilla.redhat.com/show_bug.cgi?id=1273275

app/models/miq_report_result.rb
@@ -73,17 +73,18 @@ class MiqReportResult < ActiveRecord::Base
       serializer_name = binary_blob.data_type
       serializer_name = "Marshal" unless serializer_name == "YAML"  # YAML or Marshal, for now
       serializer = serializer_name.constantize
-      MiqReport.from_hash(serializer.load(binary_blob.binary))
+      loaded = serializer.load(binary_blob.binary)
+      loaded.kind_of?(Hash) ? MiqReport.from_hash(loaded) : loaded
     elsif report.kind_of?(MiqReport)
-      return report
+      report
     else
-      return nil
+      nil
     end
   end
 
   def report_results=(value)
     self.binary_blob = BinaryBlob.new(:name => "report_results", :data_type => "YAML")
-    binary_blob.binary = YAML.dump(value.to_hash)
+    binary_blob.binary = YAML.dump(value.kind_of?(MiqReport) ? value.to_hash : value)
   end
 
   def report_html=(html)

spec/models/miq_report_result_spec.rb
@@ -1,26 +1,26 @@
 require "spec_helper"
 
 describe MiqReportResult do
-  before(:each) do
-    EvmSpecHelper.local_miq_server
+  context "persisting generated report results" do
+    before(:each) do
+      EvmSpecHelper.local_miq_server
 
-    user = FactoryGirl.create(:user_with_group)
+      user = FactoryGirl.create(:user_with_group)
 
-    5.times do |i|
-      vm = FactoryGirl.build(:vm_vmware)
-      vm.evm_owner_id = user.id               if i > 2
-      vm.miq_group_id = user.current_group.id if vm.evm_owner_id || (i > 1)
-      vm.save
+      5.times do |i|
+        vm = FactoryGirl.build(:vm_vmware)
+        vm.evm_owner_id = user.id               if i > 2
+        vm.miq_group_id = user.current_group.id if vm.evm_owner_id || (i > 1)
+        vm.save
+      end
+
+      @report_theme = 'miq'
+      @show_title   = true
+      @options = MiqReport.graph_options(600, 400)
+
+      Charting.stub(:detect_available_plugin).and_return(JqplotCharting)
     end
 
-    @report_theme = 'miq'
-    @show_title   = true
-    @options = MiqReport.graph_options(600, 400)
-
-    Charting.stub(:detect_available_plugin).and_return(JqplotCharting)
-  end
-
-  context "persisting generated report results" do
     it "should save the original report metadata and the generated table as a binary blob" do
       MiqReport.seed_report(name = "Vendor and Guest OS")
       rpt = MiqReport.where(:name => name).last
@@ -35,6 +35,44 @@ describe MiqReportResult do
       report_result.report_results.kind_of?(MiqReport).should be_true
       report_result.report_results.table.should_not be_nil
       report_result.report_results.table.data.should_not be_nil
+    end
+  end
+
+  describe "serializing and deserializing report results" do
+    it "can serialize and deserialize an MiqReport" do
+      report = FactoryGirl.build(:miq_report)
+      report_result = described_class.new
+
+      report_result.report_results = report
+
+      expect(report_result.report_results.to_hash).to eq(report.to_hash)
+    end
+
+    it "can serialize and deserialize a CSV" do
+      csv = CSV.generate { |c| c << %w(foo bar) << %w(baz qux) }
+      report_result = described_class.new
+
+      report_result.report_results = csv
+
+      expect(report_result.report_results).to eq(csv)
+    end
+
+    it "can serialize and deserialize a plain text report" do
+      txt = <<EOF
++--------------+
+|  Foo Report  |
++--------------+
+| Foo  | Bar   |
++--------------+
+| baz  | qux   |
+| quux | corge |
++--------------+
+EOF
+      report_result = described_class.new
+
+      report_result.report_results = txt
+
+      expect(report_result.report_results).to eq(txt)
     end
   end
 end