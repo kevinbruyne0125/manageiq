kevinbruyne0125/manageiq

commit 18b6d287671c0c367e51cf26a9b3490a5512bd02

bulk fix find(:all) with tweaks

app/controllers/report_controller/reports.rb
@@ -81,8 +81,8 @@ module ReportController::Reports
   def miq_report_delete
     assert_privileges("miq_report_delete")
     rpt = MiqReport.find(params[:id])
-    report_widgets = MiqWidget.all(:conditions => {:resource_id => rpt.id})
-    if report_widgets.length > 0
+
+    if rpt.widgets.exist?
       add_flash(_("Report cannot be deleted if it's being used by one or more Widgets"), :error)
       render :update do |page|
         page.replace("flash_msg_div_report_list", :partial => "layouts/flash_msg", :locals => {:div_num => "_report_list"})
@@ -195,7 +195,7 @@ module ReportController::Reports
         end
       end
 
-      @widget_nodes = MiqWidget.all(:conditions => ["resource_id = ?", @miq_report.id.to_i])
+      @widget_nodes = @miq_report.widgets.to_a
     end
 
     @sb[:tree_typ]   = "reports"

app/models/miq_report.rb
@@ -40,6 +40,7 @@ class MiqReport < ActiveRecord::Base
   belongs_to                :time_profile
   belongs_to                :miq_group
   belongs_to                :user
+  has_many                  :widgets, :as => :resource
 
   attr_accessor_that_yamls :table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name,
                            :extras, :record_id, :tl_times, :user_categories, :trend_data, :performance, :include_for_find,

app/controllers/miq_policy_controller/alert_profiles.rb
@@ -245,7 +245,7 @@ module MiqPolicyController::AlertProfiles
     alerts.each { |a| @edit[:new][:alerts][a.description] = a.id } # Build a hash for the members list box
 
     @edit[:choices] = {}
-    MiqAlert.all(:conditions => ["db = ?", @edit[:new][:mode]]).each do |a|
+    MiqAlert.where(:db => @edit[:new][:mode]).select(:id, :description).each do |a|
       @edit[:choices][a.description] = a.id           # Build a hash for the alerts to choose from
     end
 

app/controllers/ops_controller/db.rb
@@ -32,8 +32,8 @@ module OpsController::Db
       )
     elsif model == VmdbDatabaseConnection
       @zones = Zone.all.sort_by(&:name).collect { |z| [z.name, z.name] }
-      # for now we dont need this pulldown, need ot get a method that could give us a list of workers for filter pulldown
-      # @workers = MiqWorker.all(:order=>"type ASC").uniq.sort_by(&:type).collect { |w| [w.friendly_name, w.id] }
+      # for now we dont need this pulldown, need to get a method that gives us a list of workers for filter pulldown
+      # @workers = MiqWorker.all.sort_by(&:type).collect { |w| [w.friendly_name, w.id] }
     end
 
     @view, @pages = get_view(model, :filter => exp ? exp : nil) # Get the records (into a view) and the paginator

app/controllers/report_controller/widgets.rb
@@ -237,8 +237,7 @@ module ReportController::Widgets
         @view, @pages = get_view(MiqWidget, :where_clause => ["content_type=?", nodeid])
       else
         # get all widgets for passed in report id
-        # @view, @pages = get_view(MiqWidget, :where_clause=>["content_type=? AND resource_id=?",nodeid, rep_id])
-        @widget_nodes = MiqWidget.all(:conditions => ["content_type = ? AND resource_id = ?", "report", rep_id])
+        @widget_nodes = MiqWidget.where(:content_type => "report", :resource_id => rep_id)
       end
     end
 

app/models/ems_cluster.rb
@@ -310,10 +310,8 @@ class EmsCluster < ActiveRecord::Base
   end
 
   def self.get_perf_collection_object_list
-    # cl_hash = self.in_my_region.all(:include => [:tags, :taggings, :ext_management_system], :select => "name, id, ems_id").inject({}) do |h,c|
-    cl_hash = in_my_region.all(:include => [:tags, :taggings], :select => "name, id").inject({}) do |h, c|
+    cl_hash = in_my_region.includes(:tags, :taggings).select(:id, :name).each_with_object({}) do |c, h|
       h[c.id] = {:cl_rec => c, :ho_ids => c.host_ids}
-      h
     end
 
     hids = cl_hash.values.flat_map { |v| v[:ho_ids] }.compact.uniq

app/models/miq_region.rb
@@ -226,7 +226,7 @@ class MiqRegion < ActiveRecord::Base
     total_hosts   = 0
     total_sockets = 0
 
-    ExtManagementSystem.all(:order => :id).each do |e|
+    ExtManagementSystem.all.each do |e|
       vms     = e.all_vms_and_templates.count
       hosts   = e.all_hosts.count
       sockets = e.aggregate_physical_cpus

app/presenters/tree_builder_report_schedules.rb
@@ -20,9 +20,9 @@ class TreeBuilderReportSchedules < TreeBuilder
   def x_get_tree_roots(count_only, _options)
     if User.current_user.current_group.miq_user_role.name.split('-').last == 'super_administrator'
       # Super admins see all report schedules
-      objects = MiqSchedule.all(:conditions => %w(towhat=? MiqReport))
+      objects = MiqSchedule.where(:towhat => 'MiqReport')
     else
-      objects = MiqSchedule.all(:conditions => ['towhat=? AND userid=?', 'MiqReport', User.current_user.userid])
+      objects = MiqSchedule.where(:towhat => 'MiqReport', :userid => User.current_user.userid)
     end
     count_only_or_objects(count_only, objects.sort_by { |o| o.name.downcase }, 'name')
   end

commit dd424e015fb927aa059f4f8777b761abd0f40222

rename miq_reports#widgets to miq_widgets
- fix bug introduced in 18b6d287671c0c367e51cf26a9b3490a5512bd02
- fix code that protects a report from being deleted
- add specs

app/controllers/report_controller/reports.rb
@@ -82,7 +82,7 @@ module ReportController::Reports
     assert_privileges("miq_report_delete")
     rpt = MiqReport.find(params[:id])
 
-    if rpt.widgets.exist?
+    if rpt.miq_widgets.exists?
       add_flash(_("Report cannot be deleted if it's being used by one or more Widgets"), :error)
       render :update do |page|
         page.replace("flash_msg_div_report_list", :partial => "layouts/flash_msg", :locals => {:div_num => "_report_list"})
@@ -94,10 +94,11 @@ module ReportController::Reports
         audit = {:event => "report_record_delete", :message => "[#{rpt_name}] Record deleted", :target_id => rpt.id, :target_class => "MiqReport", :userid => session[:userid]}
         rpt.destroy
       rescue StandardError => bang
-        add_flash(_("%{model} \"%{name}\": Error during '%{task}': ") % {:model => ui_lookup(:model => "MiqReport"), :name => rpt_name, :task => task} << bang.message, :error)
+        add_flash(_("%{model} \"%{name}\": Error during '%{task}': ") % {:model => ui_lookup(:model => "MiqReport"), :name => rpt_name, :task => 'miq_report_delete'} << bang.message, :error)
         render :update do |page|
           page.replace("flash_msg_div_report_list", :partial => "layouts/flash_msg", :locals => {:div_num => "_report_list"})
         end
+        return
       else
         AuditEvent.success(audit)
         add_flash(_("%{model} \"%{name}\": Delete successful") % {:model => ui_lookup(:model => "MiqReport"), :name => rpt_name})
@@ -187,7 +188,7 @@ module ReportController::Reports
       schedules = schedules.where(:userid => current_userid) unless super_admin_user?
       @schedules = schedules.select { |s| s.filter.exp["="]["value"].to_i == @miq_report.id.to_i }.sort_by(&:name)
 
-      @widget_nodes = @miq_report.widgets.to_a
+      @widget_nodes = @miq_report.miq_widgets.to_a
     end
 
     @sb[:tree_typ]   = "reports"

app/models/miq_report.rb
@@ -40,7 +40,7 @@ class MiqReport < ActiveRecord::Base
   belongs_to                :time_profile
   belongs_to                :miq_group
   belongs_to                :user
-  has_many                  :widgets, :as => :resource
+  has_many                  :miq_widgets, :as => :resource
 
   attr_accessor_that_yamls :table, :sub_table, :filter_summary, :extras, :ids, :scoped_association, :html_title, :file_name,
                            :extras, :record_id, :tl_times, :user_categories, :trend_data, :performance, :include_for_find,

spec/controllers/miq_report_controller/reports_spec.rb
@@ -40,4 +40,45 @@ describe ReportController, "::Reports" do
       end
     end
   end
+
+  describe "#miq_report_delete" do
+    before do
+      EvmSpecHelper.local_miq_server # timezone stuff
+      login_as FactoryGirl.create(:user, :features => :miq_report_delete)
+      request.env['HTTP_REFERER'] = session['referer'] = controller.controller_name+"/" # work around referer security
+    end
+
+    it "deletes the report" do
+      FactoryGirl.create(:miq_report)
+      report = FactoryGirl.create(:miq_report, :rpt_type => "Custom")
+      session['sandboxes'] = {
+        controller.controller_name => { :active_tree => 'report_1',
+                      :trees => {'report_1' => {:active_node => "xx-0_xx-0-0_rep-#{report.id}"}}
+        }
+      }
+
+      get :x_button, :id => report.id, :pressed => 'miq_report_delete'
+      expect(response.status).to eq(200)
+      expect(MiqReport.find_by(:id => report.id)).to be_nil
+    end
+
+    it "cant delete default reports" do
+      FactoryGirl.create(:miq_report)
+      report = FactoryGirl.create(:miq_report, :rpt_type => "Default")
+      session['sandboxes'] = {
+        controller.controller_name => { :active_tree => 'report_1',
+                      :trees => {'report_1' => {:active_node => "xx-0_xx-0-0_rep-#{report.id}"}}
+        }
+      }
+
+      get :x_button, :id => report.id, :pressed => 'miq_report_delete'
+      expect(response.status).to eq(200)
+      expect(MiqReport.find_by(:id => report.id)).not_to be_nil
+    end
+
+    # it "fails if widgets exist" do
+    #   report = FactoryGirl.create(:miq_report)
+    #   FactoryGirl.create(:miq_widget, :resource => report)
+    # end
+  end
 end