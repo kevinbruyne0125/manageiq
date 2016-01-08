kevinbruyne0125/manageiq

commit 49dc2581fed7acfc50c5a7d4c984289de0031906

path_to_my_worker_settings

lib/vmdb/config.rb
@@ -251,9 +251,7 @@ module VMDB
       klass = Object.const_get(klass) unless klass.class == Class
 
       # find the key for the class and set the value
-      keys = klass.path_to_my_worker_settings.dup
-      keys.unshift(:workers)
-      keys += setting.to_miq_a
+      keys = klass.config_settings_path + settings.to_miq_a
       config.store_path(keys, value)
     end
 

app/models/miq_schedule_worker/runner.rb
@@ -67,7 +67,7 @@ class MiqScheduleWorker::Runner < MiqWorker::Runner
     keys    = [keys] unless keys.kind_of?(Array)
     value   = worker_settings.fetch_path(keys)
     value ||= begin
-      fq_keys = [:workers] + @worker.class.path_to_my_worker_settings + keys
+      fq_keys = @worker.class.config_settings_path + keys
       v = VMDB::Config.new("vmdb").template_configuration.fetch_path(fq_keys)
       v = v.to_i_with_method if v.number_with_method?
       v

commit 7c0e74741ccb1578ce321d9e91af39540b3879e3

Fix: Can now set worker configuration values in UI
Bug introduced in 49dc2581fed7acfc50c5a7d4c984289de0031906

https://bugzilla.redhat.com/show_bug.cgi?id=1296638#c2

lib/vmdb/config.rb
@@ -251,7 +251,7 @@ module VMDB
       klass = Object.const_get(klass) unless klass.class == Class
 
       # find the key for the class and set the value
-      keys = klass.config_settings_path + settings.to_miq_a
+      keys = klass.config_settings_path + setting.to_miq_a
       config.store_path(keys, value)
     end
 

spec/lib/vmdb/config_spec.rb
@@ -71,4 +71,15 @@ describe VMDB::Config do
       expect(error[1]).to match(/\AFile contents are malformed/)
     end
   end
+
+  describe "set_worker_setting!" do
+    it "stores path" do
+      config = VMDB::Config.new("vmdb")
+      config.set_worker_setting!(:MiqEmsMetricsCollectorWorker, :memory_threshold, "250.megabytes")
+
+      fq_keys = [:workers, :worker_base, :queue_worker_base, :ems_metrics_collector_worker] + [:memory_threshold]
+      v = config.config.fetch_path(fq_keys).to_i_with_method
+      expect(v).to eq(250.megabytes)
+    end
+  end
 end