kevinbruyne0125/manageiq

commit b9a542661dbd3f82f6677ab94c0c1d1d9b503a7b

reduce redundancy in miq_storage

app/models/miq_storage_metric.rb
@@ -213,7 +213,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called directly from MiqStorageMetric.
   #
   def self.sub_classes
-    select('DISTINCT type').collect { |c| c.type.constantize }
+    sub_class_names.map(&:constantize)
   end
 
   #
@@ -221,7 +221,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called directly from MiqStorageMetric.
   #
   def self.derived_metrics_class_names
-    select('DISTINCT type').collect { |c| c.type.constantize.derived_metrics_class_name }
+    sub_classes.map(&:derived_metrics_class_name)
   end
 
   #
@@ -229,7 +229,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called directly from MiqStorageMetric.
   #
   def self.derived_metrics_classes
-    select('DISTINCT type').collect { |c| c.type.constantize.derived_metrics_class_name.constantize }
+    derived_metrics_class_names.map(&:constantize)
   end
 
   #
@@ -237,7 +237,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called directly from MiqStorageMetric.
   #
   def self.metrics_rollup_class_names
-    select('DISTINCT type').collect { |c| c.type.constantize.metrics_rollup_class_name }
+    subclasses.map(&:metrics_rollup_class_name)
   end
 
   #
@@ -245,7 +245,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called directly from MiqStorageMetric.
   #
   def self.metrics_rollup_classes
-    select('DISTINCT type').collect { |c| c.type.constantize.metrics_rollup_class_name.constantize }
+    metrics_rollup_class_names.map(&:constantize)
   end
 
   #
@@ -254,8 +254,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Constant defined in subclass.
   #
   def self.derived_metrics_class_name
-    return nil unless self.const_defined?(:DERIVED_METRICS_CLASS_NAME)
-    self::DERIVED_METRICS_CLASS_NAME
+    self::DERIVED_METRICS_CLASS_NAME unless self.const_defined?(:DERIVED_METRICS_CLASS_NAME)
   end
 
   #
@@ -263,8 +262,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called from subclass of MiqStorageMetric.
   #
   def self.derived_metrics_class
-    return nil if derived_metrics_class_name.nil?
-    derived_metrics_class_name.constantize
+    derived_metrics_class_name.try!(:constantize)
   end
 
   #
@@ -279,8 +277,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Constant defined in subclass.
   #
   def self.metrics_rollup_class_name
-    return nil unless self.const_defined?(:METRICS_ROLLUP_CLASS_NAME)
-    self::METRICS_ROLLUP_CLASS_NAME
+    self::METRICS_ROLLUP_CLASS_NAME if self.const_defined?(:METRICS_ROLLUP_CLASS_NAME)
   end
 
   #
@@ -288,8 +285,7 @@ class MiqStorageMetric < ActiveRecord::Base
   # Called from subclass of MiqStorageMetric.
   #
   def self.metrics_rollup_class
-    return nil if metrics_rollup_class_name.nil?
-    metrics_rollup_class_name.constantize
+    metrics_rollup_class_name.try!(:constantize)
   end
 
   #

commit e54022031224f5e3d673579c400361140b07da4f

MiqStorage.metrics_rollup_class instantiated only
Fixes bug introduced in b9a542661dbd3f82f6677ab94c0c1d1d9b503a7b

need to only return the metrics that are instantiated

app/models/miq_storage_metric.rb
@@ -96,7 +96,7 @@ class MiqStorageMetric < ApplicationRecord
   end
 
   def metrics_rollups_by_rollup_type(rollup_type)
-    miq_metrics_rollups.where(rollup_type => rollup_type).to_a
+    miq_metrics_rollups.where(:rollup_type => rollup_type).to_a
   end
 
   def addDerivedMetric(derivedMetrics)
@@ -222,7 +222,7 @@ class MiqStorageMetric < ApplicationRecord
   # Called directly from MiqStorageMetric.
   #
   def self.metrics_rollup_class_names
-    subclasses.map(&:metrics_rollup_class_name).compact
+    sub_classes.map(&:metrics_rollup_class_name).compact
   end
 
   #

spec/models/miq_storage_metric_spec.rb
@@ -62,18 +62,14 @@ describe MiqStorageMetric do
   describe '.metrics_rollup_class_names' do
     it "works" do
       OntapAggregateMetric.create
-      expect(MiqStorageMetric.metrics_rollup_class_names).to match_array(
-        %w(OntapAggregateMetricsRollup OntapDiskMetricsRollup OntapLunMetricsRollup
-           OntapSystemMetricsRollup OntapVolumeMetricsRollup))
+      expect(MiqStorageMetric.metrics_rollup_class_names).to match_array(%w(OntapAggregateMetricsRollup))
     end
   end
 
   describe '.metrics_rollup_classes' do
     it "works" do
       OntapAggregateMetric.create
-      expect(MiqStorageMetric.metrics_rollup_classes).to match_array([
-        OntapAggregateMetricsRollup, OntapDiskMetricsRollup, OntapLunMetricsRollup,
-        OntapSystemMetricsRollup, OntapVolumeMetricsRollup])
+      expect(MiqStorageMetric.metrics_rollup_classes).to match_array([OntapAggregateMetricsRollup])
     end
   end
 
@@ -130,6 +126,14 @@ describe MiqStorageMetric do
       stub_server_configuration(:storage => {:metrics_history => {}})
       OntapAggregateMetric.create
       MiqStorageMetric.purge_all_timer
+    end
+  end
+
+  describe '.metrics_rollup_by_rollup_type' do
+    it "works" do
+      met = OntapDiskMetric.create()
+      rollups = 2.times.map { met.miq_metrics_rollups.create(:rollup_type => "hourly") }
+      expect(met.metrics_rollups_by_rollup_type("hourly")).to match_array(rollups)
     end
   end
 end