kevinbruyne0125/manageiq

commit 46032b28ef05b3b74dbe3f1cab29378d9d1b64c4

Converted service dialog import from SlickGrid to PatternFly

app/controllers/miq_ae_customization_controller.rb
@@ -45,15 +45,14 @@ class MiqAeCustomizationController < ApplicationController
   end
 
   def upload_import_file
-    redirect_options = {:action => :review_import}
-
     if params[:upload].nil? || params[:upload][:file].blank?
       add_flash(_("Use the browse button to locate an import file"), :warning)
     else
       begin
-        import_file_upload_id = dialog_import_service.store_for_import(params[:upload][:file].read)
+        import_file = dialog_import_service.store_for_import(params[:upload][:file].read)
+        @import_file_upload_id = import_file.id
+        @import = import_file.service_dialog_list
         add_flash(_("Import file was uploaded successfully"), :success)
-        redirect_options[:import_file_upload_id] = import_file_upload_id
       rescue DialogImportValidator::ImportNonYamlError
         add_flash(_("Error: the file uploaded is not of the supported format"), :error)
       rescue DialogImportValidator::ParsedNonDialogYamlError
@@ -63,54 +62,25 @@ class MiqAeCustomizationController < ApplicationController
       end
     end
 
-    redirect_options[:message] = @flash_array.first.to_json
-
-    redirect_to redirect_options
+    replace_right_cell(x_node)
   end
 
   def import_service_dialogs
-    import_file_upload = ImportFileUpload.find_by(:id => params[:import_file_upload_id])
+    if params[:commit] == _('Commit')
+      import_file_upload = ImportFileUpload.find_by(:id => params[:import_file_upload_id])
 
-    if import_file_upload
-      dialog_import_service.import_service_dialogs(import_file_upload, params[:dialogs_to_import])
-      add_flash(_("Service dialogs imported successfully"), :success)
+      if import_file_upload
+        dialog_import_service.import_service_dialogs(import_file_upload, params[:dialogs_to_import])
+        add_flash(_("Service dialogs imported successfully"), :success)
+      else
+        add_flash(_("Error: ImportFileUpload expired"), :error)
+      end
     else
-      add_flash(_("Error: ImportFileUpload expired"), :error)
+      dialog_import_service.cancel_import(params[:import_file_upload_id])
+      add_flash(_("Service dialog import cancelled"), :success)
     end
 
-    respond_to do |format|
-      format.js { render :json => @flash_array.to_json, :status => 200 }
-    end
-  end
-
-  def dialog_accordion_json
-    accordion_json = {:locals_for_render => dialog_build_tree.locals_for_render}.to_json
-
-    respond_to do |format|
-      format.json { render :json => accordion_json, :status => 200 }
-    end
-  end
-
-  def review_import
-    @import_file_upload_id = params[:import_file_upload_id]
-    @message = params[:message]
-  end
-
-  def cancel_import
-    dialog_import_service.cancel_import(params[:import_file_upload_id])
-    add_flash(_("Service dialog import cancelled"), :success)
-
-    respond_to do |format|
-      format.js { render :json => @flash_array.to_json, :status => 200 }
-    end
-  end
-
-  def service_dialog_json
-    import_file_upload_json = ImportFileUpload.find(params[:import_file_upload_id]).service_dialog_json
-
-    respond_to do |format|
-      format.json { render :json => import_file_upload_json }
-    end
+    replace_right_cell(x_node)
   end
 
   def export_service_dialogs
@@ -248,6 +218,7 @@ class MiqAeCustomizationController < ApplicationController
       presenter[:clear_tree_cookies] = "edit_treeOpenStatex"
     end
     rebuild_toolbars(presenter)
+    get_node_info
     setup_presenter_based_on_active_tree(nodetype, presenter)
     set_right_cell_text(presenter)
     handle_bottom_cell(presenter)

spec/controllers/miq_ae_customization_controller_spec.rb
@@ -242,39 +242,41 @@ describe MiqAeCustomizationController do
 
     before do
       bypass_rescue
+      allow(controller).to receive(:get_node_info)
+      controller.instance_variable_set(:@sb,
+                                       :trees       => {:dialog_import_export_tree => {:active_node => "root"}},
+                                       :active_tree => :dialog_import_export_tree)
     end
 
     shared_examples_for "MiqAeCustomizationController#upload_import_file that does not upload a file" do
       it "redirects with a warning message" do
         post :upload_import_file, :params => params, :xhr => true
-        expect(response).to redirect_to(
-          :action  => :review_import,
-          :message => {:message => "Use the browse button to locate an import file", :level => :warning}.to_json
-        )
+        expect(controller.instance_variable_get(:@flash_array))
+          .to include(:message => "Use the browse button to locate an import file", :level => :warning)
       end
     end
 
     context "when an upload file is given" do
+      let(:import_file_upload) { double("ImportFileUpload") }
       let(:filename) { "filename" }
       let(:file) { fixture_file_upload("files/dummy_file.yml", "text/yml") }
       let(:params) { {:upload => {:file => file}} }
 
       before do
         allow(DialogImportService).to receive(:new).and_return(dialog_import_service)
+        allow(import_file_upload).to receive(:id).and_return(123)
+        allow(import_file_upload).to receive(:service_dialog_list)
       end
 
       context "when the dialog importer does not raise an error" do
         before do
-          allow(dialog_import_service).to receive(:store_for_import).with("the yaml data\n").and_return(123)
+          allow(dialog_import_service).to receive(:store_for_import).with("the yaml data\n").and_return(import_file_upload)
         end
 
         it "redirects to review_import with an import file upload id" do
           post :upload_import_file, :params => params, :xhr => true
-          expect(response).to redirect_to(
-            :action                => :review_import,
-            :import_file_upload_id => 123,
-            :message               => {:message => "Import file was uploaded successfully", :level => :success}.to_json
-          )
+          expect(controller.instance_variable_get(:@flash_array))
+            .to include(:message => "Import file was uploaded successfully", :level => :success)
         end
 
         it "imports the dialogs" do
@@ -291,13 +293,8 @@ describe MiqAeCustomizationController do
 
         it "redirects with an error message" do
           post :upload_import_file, :params => params, :xhr => true
-          expect(response).to redirect_to(
-            :action  => :review_import,
-            :message => {
-              :message => "Error: the file uploaded is not of the supported format",
-              :level   => :error
-            }.to_json
-          )
+          expect(controller.instance_variable_get(:@flash_array))
+            .to include(:message => "Error: the file uploaded is not of the supported format", :level => :error)
         end
       end
 
@@ -309,13 +306,9 @@ describe MiqAeCustomizationController do
 
         it "redirects with an error message" do
           post :upload_import_file, :params => params, :xhr => true
-          expect(response).to redirect_to(
-            :action  => :review_import,
-            :message => {
-              :message => "Error during upload: incorrect Dialog format, only service dialogs can be imported",
-              :level   => :error
-            }.to_json
-          )
+          expect(controller.instance_variable_get(:@flash_array))
+            .to include(:message => "Error during upload: incorrect Dialog format, only service dialogs can be imported",
+                        :level   => :error)
         end
       end
 
@@ -327,13 +320,9 @@ describe MiqAeCustomizationController do
 
         it "redirects with an error message" do
           post :upload_import_file, :params => params, :xhr => true
-          expect(response).to redirect_to(
-            :action  => :review_import,
-            :message => {
-              :message => "Error during upload: one of the DialogField types is not supported",
-              :level   => :error
-            }.to_json
-          )
+          expect(controller.instance_variable_get(:@flash_array))
+            .to include(:message => "Error during upload: one of the DialogField types is not supported",
+                        :level   => :error)
         end
       end
     end
@@ -355,12 +344,14 @@ describe MiqAeCustomizationController do
     include_context "valid session"
 
     let(:dialog_import_service) { double("DialogImportService") }
-    let(:params) { {:import_file_upload_id => "123", :dialogs_to_import => ["potato"]} }
+    let(:params) { {:import_file_upload_id => "123", :dialogs_to_import => ["potato"], :commit => 'Commit'} }
+    let(:import_file_upload) { double("ImportFileUpload") }
 
     before do
       bypass_rescue
       allow(ImportFileUpload).to receive(:find_by).with(:id => "123").and_return(import_file_upload)
       allow(DialogImportService).to receive(:new).and_return(dialog_import_service)
+      allow(controller).to receive(:get_node_info)
     end
 
     shared_examples_for "MiqAeCustomizationController#import_service_dialogs" do
@@ -371,7 +362,6 @@ describe MiqAeCustomizationController do
     end
 
     context "when the import file upload exists" do
-      let(:import_file_upload) { double("ImportFileUpload") }
 
       before do
         allow(dialog_import_service).to receive(:import_service_dialogs)
@@ -386,7 +376,8 @@ describe MiqAeCustomizationController do
 
       it "returns the flash message" do
         post :import_service_dialogs, :params => params, :xhr => true
-        expect(response.body).to eq([{:message => "Service dialogs imported successfully", :level => :success}].to_json)
+        expect(controller.instance_variable_get(:@flash_array))
+          .to include(:message => "Service dialogs imported successfully", :level => :success)
       end
     end
 
@@ -397,74 +388,31 @@ describe MiqAeCustomizationController do
 
       it "returns the flash message" do
         post :import_service_dialogs, :params => params, :xhr => true
-        expect(response.body).to eq([{:message => "Error: ImportFileUpload expired", :level => :error}].to_json)
+        expect(controller.instance_variable_get(:@flash_array))
+          .to include(:message => "Error: ImportFileUpload expired", :level => :error)
       end
     end
-  end
 
-  describe "#review_import" do
-    include_context "valid session"
+    context 'cancel import' do
+      let(:params) { {:import_file_upload_id => "123", :dialogs_to_import => ["potato"]} }
 
-    let(:params) { {:import_file_upload_id => "123", :message => "the message"} }
+      it "cancels the import" do
+        expect(dialog_import_service).to receive(:cancel_import).with("123")
+        post :import_service_dialogs, :params => params, :xhr => true
+      end
 
-    before do
-      bypass_rescue
-    end
+      it "returns a 200" do
+        expect(dialog_import_service).to receive(:cancel_import).with("123")
+        post :import_service_dialogs, :params => params, :xhr => true
+        expect(response.status).to eq(200)
+      end
 
-    it "assigns the import file upload id" do
-      get :review_import, :params => params
-      expect(assigns(:import_file_upload_id)).to eq("123")
-    end
-
-    it "assigns the message" do
-      get :review_import, :params => params
-      expect(assigns(:message)).to eq("the message")
-    end
-  end
-
-  describe "#cancel_import" do
-    include_context "valid session"
-
-    let(:params) { {:import_file_upload_id => "123"} }
-    let(:dialog_import_service) { double("DialogImportService") }
-
-    before do
-      bypass_rescue
-      allow(DialogImportService).to receive(:new).and_return(dialog_import_service)
-      allow(dialog_import_service).to receive(:cancel_import)
-    end
-
-    it "cancels the import" do
-      expect(dialog_import_service).to receive(:cancel_import).with("123")
-      post :cancel_import, :params => params, :xhr => true
-    end
-
-    it "returns a 200" do
-      post :cancel_import, :params => params, :xhr => true
-      expect(response.status).to eq(200)
-    end
-
-    it "returns the flash messages" do
-      post :cancel_import, :params => params, :xhr => true
-      expect(response.body).to eq([{:message => "Service dialog import cancelled", :level => :success}].to_json)
-    end
-  end
-
-  describe "#service_dialog_json" do
-    include_context "valid session"
-
-    let(:params) { {:import_file_upload_id => "123"} }
-    let(:import_file_upload) { double("ImportFileUpload") }
-
-    before do
-      bypass_rescue
-      allow(ImportFileUpload).to receive(:find).with("123").and_return(import_file_upload)
-      allow(import_file_upload).to receive(:service_dialog_json).and_return("the service dialog json")
-    end
-
-    it "returns the json" do
-      get :service_dialog_json, :params => params, :xhr => true
-      expect(response.body).to eq("the service dialog json")
+      it "returns the flash messages" do
+        expect(dialog_import_service).to receive(:cancel_import).with("123")
+        post :import_service_dialogs, :params => params, :xhr => true
+        expect(controller.instance_variable_get(:@flash_array))
+          .to include(:message => "Service dialog import cancelled", :level => :success)
+      end
     end
   end
 

app/assets/javascripts/application.js
@@ -27,7 +27,6 @@
 //= require miq_application
 //= require miq_change_stored_password
 //= require miq_dynatree_replacement
-//= require dialog_import_export
 //= require automate_import_export
 //= require dialog_field_refresh
 //= require excanvas

app/assets/javascripts/dialog_import_export.js
@@ -1,71 +1,0 @@
-//= require import
-
-var renderServiceDialogJson = function(rows_json, importFileUploadId) {
-  var statusFormatter = function(row, cell, value, columnDef, dataContext) {
-    var status_img = "<img src=\"" + dataContext.status_icon + "\" />";
-
-    return status_img + dataContext.status;
-  };
-
-  var dataview = new Slick.Data.DataView({inlineFilters: true});
-
-  var checkboxSelector = new Slick.CheckboxSelectColumn({
-    cssClass: "import-checkbox",
-  });
-
-  var columns = [
-    checkboxSelector.getColumnDefinition(),
-    {
-      id: "name",
-      name: __("Service Dialog Name"),
-      field: "name",
-      width: 300
-    }, {
-      id: "status",
-      name: __("Status"),
-      field: "status",
-      width: 300,
-      formatter: statusFormatter
-    }
-  ];
-
-  dataview.beginUpdate();
-  dataview.setItems(rows_json);
-  dataview.endUpdate();
-
-  var grid = new Slick.Grid("#import-grid", dataview, columns, {enableColumnReorder: false});
-
-  grid.setSelectionModel(new Slick.RowSelectionModel({selectActiveRow: false}));
-  grid.registerPlugin(checkboxSelector);
-
-  $('#import_file_upload_id').val(importFileUploadId);
-  $('.import-data').show();
-  $('.import-or-export').hide();
-
-  var rowsToSelect = [];
-  $.each(grid.getData().getItems(), function(index, item) {
-    if (item.status_icon.indexOf('/equal-green') > -1) {
-      rowsToSelect.push(item.id);
-    }
-  });
-
-  grid.setSelectedRows(rowsToSelect);
-  grid.invalidate();
-  grid.render();
-
-  setUpImportClickHandlers('import_service_dialogs', grid, function() {
-    $.get('dialog_accordion_json', function(data) {
-      ManageIQ.dynatreeReplacement.replace(data.locals_for_render);
-    });
-  });
-};
-
-var getAndRenderServiceDialogJson = function(importFileUploadId, message) {
-  $('.hidden-import-file-upload-id').val(importFileUploadId);
-
-  $.getJSON("service_dialog_json?import_file_upload_id=" + importFileUploadId).done(function(rows_json) {
-    renderServiceDialogJson(rows_json, importFileUploadId);
-  });
-
-  showSuccessMessage(JSON.parse(message).message);
-};

app/assets/stylesheets/application.css
@@ -7,7 +7,6 @@
  *= require main
  *= require jquery.jqplot
  *= require automate_import_export
- *= require dialog_import_export
  *= require dialog_fields
  *= require angular
  *= require miq_timeline

app/assets/stylesheets/dialog_import_export.css
@@ -1,24 +1,0 @@
-#import-grid {
-  height: 350px;
-  width: 680px;
-}
-
-#toggle-all {
-  margin-bottom: 10px;
-}
-
-.import-cancel, .import-commit {
-  cursor: pointer;
-}
-
-.import-data {
-  display: none;
-}
-
-.import-flash-message {
-  display: none;
-}
-
-.import-hidden-iframe {
-  display: none;
-}

app/models/import_file_upload.rb
@@ -5,9 +5,9 @@ class ImportFileUpload < ApplicationRecord
     MiqPolicy.import_from_array(uploaded_yaml_content, :preview => true)
   end
 
-  def service_dialog_json
+  def service_dialog_list
     sorted_service_dialogs = uploaded_yaml_content.sort_by { |service_dialog| service_dialog["label"].downcase }
-    service_dialogs = sorted_service_dialogs.collect.with_index do |dialog, index|
+    sorted_service_dialogs.collect.with_index do |dialog, index|
       status_icon = Dialog.exists?(:label => dialog["label"]) ? "checkmark" : "equal-green"
       status = determine_status(status_icon)
 
@@ -18,8 +18,6 @@ class ImportFileUpload < ApplicationRecord
         :status      => status
       }
     end
-
-    service_dialogs.to_json
   end
 
   def widget_list

app/views/miq_ae_customization/_dialog_import_export.html.haml
@@ -1,63 +1,78 @@
-%iframe.import-hidden-iframe{:name => "upload_target"}
+= render :partial => 'layouts/flash_msg'
 
-.import-flash-message
-  .alert
-    %span.icon-placeholder
-      %span.pficon
-      %span.pficon
-    %strong.message
+- if @import
+  %h3
+    = _('Import Service Dialogs')
+  = form_tag('import_service_dialogs', :id => 'import-service-dialogs', :remote => true) do
+    %table#import-grid.table.table-striped.table-bordered
+      %thead
+        %tr
+          %th.narrow
+            %input{:type => 'checkbox', :id => 'check-all'}
+          %th
+            = _('Dialog name')
+          %th
+            = _('Status')
+      %tbody
+        - @import.each do |item|
+          %tr
+            %td.narrow
+              %input{:type => 'checkbox', :name => 'dialogs_to_import[]', :value => item[:name]}
+            %td
+              = item[:name]
+            %td
+              %img{:src => item[:status_icon]}
+              &nbsp;
+              = item[:status]
+    .pull-right
+      = hidden_field_tag(:import_file_upload_id, @import_file_upload_id, :class => "hidden-import-file-upload-id")
+      = submit_tag(_('Commit'), :alt => _("Commit Changes"), :class => "btn btn-primary import-commit")
+      = submit_tag(_('Cancel'), :alt => _("Cancel Changes"), :class => "btn btn-default import-cancel")
 
-.import-or-export
+- else
   %h3
     = _('Import')
-  = form_tag({:action => "upload_import_file"},
-             :class     => "form-horizontal",
-             :target    => "upload_target",
-             :multipart => true,
-             :method    => :post) do
+  = form_tag('upload_import_file', :id => 'upload-service-dialogs', :remote => true) do
     .form-group
-      .col-md-4
-        = file_field("upload", "file",
-                     :class             => "lifestyle",
-                     "data-buttonName"  => "btn-default")
-        :javascript
-          $(":file").filestyle({icon: false, placeholder: "No file chosen"});
-      .col-md-6
-        = button_tag(_("Upload"), :id => "upload_service_dialog_import", :class => "btn btn-default", :action => "submit")
+      .row
+        .col-md-4
+          = file_field("upload", "file", :class => "lifestyle", "data-buttonName" => "btn-default")
+          :javascript
+            $(":file").filestyle({
+              icon: false,
+              placeholder: "No file chosen"
+            });
+        .col-md-6
+          = button_tag(_("Upload"), :id => "upload_service_dialog_import", :class => "btn btn-default", :action => "submit")
 
   %hr
-  %h3= _('Export')
-  = form_tag({:action => "export_service_dialogs"},
-             :method => :get) do
-    = select_tag("service_dialogs[]",
-      options_for_select(@dialog_exports),
-      :class    => "service-dialog-export",
-      :multiple => true)
-    = button_tag(_("Export"),
-      :class => "btn btn-primary",
-      :type  => "submit")
-
-.import-data
-  %form#import-form
-    %h3= _('Import Service Dialogs')
-
-    #import-grid{:style => "width: #{@winW - 50}px; height: #{center_div_height + 5}px; cursor: hand; overflow-x: auto; overflow-y: auto;"}
-
-    %table{:width => "100%"}
-      %tr
-        %td{:align => "right"}
-          #buttons
-            = hidden_field_tag(:import_file_upload_id, '', :class => "hidden-import-file-upload-id")
-            = button_tag(:type => "button", :alt => "Commit Changes", :class => "btn btn-primary import-commit") do
-              = _('Commit')
-            = button_tag(:type => "button", :alt => "Cancel Changes", :class => "btn btn-default import-cancel") do
-              = _('Cancel')
+  %h3
+    = _('Export')
+  = form_tag('export_service_dialogs', :method => :get) do
+    = select_tag("service_dialogs[]", options_for_select(@dialog_exports), :class => "service-dialog-export", :multiple => true)
+    = button_tag(_("Export"), :class => "btn btn-primary", :type => "submit")
 
 :javascript
-  $(function() {
-    $('#upload_service_dialog_import').click(function() {
-      miqSparkleOn();
+  $(function () {
+    $("#toolbar").hide();
+
+    $('#upload-service-dialogs').submit(function (e) {
+       miqSparkleOn();
+       var data = new FormData(this);
+       miqJqueryRequest($(this).prop('action'), {
+         data:        data,
+         processData: false,
+         contentType: false,
+         cache:       false
+       });
+       e.preventDefault();
+     });
+
+    $('#check-all').click(function (e) {
+      $("table#import-grid tbody input[type='checkbox']").prop('checked', $(this).prop('checked'));
     });
 
-    ImportSetup.listenForPostMessages(getAndRenderServiceDialogJson);
+    $("#import-service-dialogs").submit(function (e) {
+      miqSparkleOn();
+    });
   });

commit 5b622fd72c8aca627dbbc696bbd326e2f7d9d5c1

Moved call to get_node_info into import specific methods.
Adding call to get_node_info in replace_right_cell was causing it to be called twice once from specific methods and once from replace_right_cell, this was also causing issues when going to add/edit screens in Buttons accordion. Issue was introduced in commit 46032b28ef05b3b74dbe3f1cab29378d9d1b64c4

Fixes #8195

app/controllers/miq_ae_customization_controller.rb
@@ -61,7 +61,7 @@ class MiqAeCustomizationController < ApplicationController
         add_flash(_("Error during upload: one of the DialogField types is not supported"), :error)
       end
     end
-
+    get_node_info
     replace_right_cell(x_node)
   end
 
@@ -79,7 +79,7 @@ class MiqAeCustomizationController < ApplicationController
       dialog_import_service.cancel_import(params[:import_file_upload_id])
       add_flash(_("Service dialog import cancelled"), :success)
     end
-
+    get_node_info
     replace_right_cell(x_node)
   end
 
@@ -218,7 +218,7 @@ class MiqAeCustomizationController < ApplicationController
       presenter[:clear_tree_cookies] = "edit_treeOpenStatex"
     end
     rebuild_toolbars(presenter)
-    get_node_info
+
     setup_presenter_based_on_active_tree(nodetype, presenter)
     set_right_cell_text(presenter)
     handle_bottom_cell(presenter)

spec/controllers/miq_ae_customization_controller_spec.rb
@@ -476,4 +476,27 @@ describe MiqAeCustomizationController do
       end
     end
   end
+
+  context "#x_button" do
+    before :each do
+      controller.instance_variable_set(:@sb, :applies_to_class => "EmsCluster")
+      allow(controller).to receive(:role_allows).and_return(true)
+      session[:sandboxes] = {
+        "miq_ae_customization" => {
+          :applies_to_class => "EmsCluster",
+          :trees            => {
+            :ab_tree => {:active_node => "-ub-EmsCluster"}
+          },
+          :active_tree      => :ab_tree
+        }
+      }
+    end
+
+    it "it should not call get_node_info when building new group add screen" do
+      expect(controller).to_not receive(:get_node_info)
+      post :x_button, :params => {:pressed => "ab_group_new"}
+      expect(response.status).to eq(200)
+      expect(response.body).to include("main_div")
+    end
+  end
 end