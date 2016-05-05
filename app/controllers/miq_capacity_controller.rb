kevinbruyne0125/manageiq

commit 59f0c5fdf553503421c7cb3394d2d40991b95622

MiqCapacityController extract filter_typ
all sections are setting options (not <Choose>)
loading some vms based upon filter_typ
and then clearing the other variables

app/controllers/miq_capacity_controller.rb
@@ -156,7 +156,6 @@ class MiqCapacityController < ApplicationController
   def planning_option_changed
     vms = nil
     if params[:filter_typ]
-      @sb[:planning][:options][:filter_typ] = params[:filter_typ] == "<Choose>" ? nil : params[:filter_typ]
       @sb[:planning][:options][:filter_value] = nil
       if params[:filter_typ] == "all"
         vms = find_filtered(Vm).sort_by { |v| v.name.downcase }
@@ -169,7 +168,7 @@ class MiqCapacityController < ApplicationController
         @sb[:planning][:options][:filter_value] = params[:filter_value]
         vms = []
         if @sb[:planning][:options][:filter_value]
-          case @sb[:planning][:options][:filter_typ]
+          case params[:filter_typ]
           when "host"
             vms, count = Host.find(@sb[:planning][:options][:filter_value]).find_filtered_children("vms")
           when "ems"
@@ -183,6 +182,7 @@ class MiqCapacityController < ApplicationController
       end
     end
     @sb[:planning][:vms] = vms ? vms.each_with_object({}) { |v, h| h[v.id.to_s] = v.name } : nil
+    @sb[:planning][:options][:filter_typ] = params[:filter_typ] == "<Choose>" ? nil : params[:filter_typ]
     @sb[:planning][:options][:chosen_vm] = params[:chosen_vm] == "<Choose>" ? nil : params[:chosen_vm]
     @sb[:planning][:options][:days] = params[:trend_days].to_i if params[:trend_days]
     @sb[:planning][:options][:vm_mode] = VALID_PLANNING_VM_MODES[params[:vm_mode]] if params[:vm_mode]

commit 2b324340ade0d0cd06233b965c1c50333e043738

Fix broken Optimize/Planning.
The wizard for selecting VMs based on different criteria was broken by
59f0c5f in #7406.

The assumption that params[:filter_typ] is sent with
params[:filter_value] is wrong. On the contrary, each of those is sent
in a separate transaction.

This causes the wizzard never to collect the values and therefor report
cannot be created.

Fix this by updating only the values that are actually sent in the
transtaction.

app/controllers/miq_capacity_controller.rb
@@ -154,29 +154,40 @@ class MiqCapacityController < ApplicationController
     end
   end
 
+  def planning_wizard_get_vms_records(filter_type, filter_value)
+     case filter_type
+     when "all"     then find_filtered(Vm).sort_by { |v| v.name.downcase }
+     when "host"    then Host.find(filter_value).vms
+     when "ems"     then ExtManagementSystem.find(filter_value).vms
+     when "cluster" then EmsCluster.find(filter_value).all_vms
+     when "filter"  then MiqSearch.find(filter_value).results(:userid => current_userid)
+     else []
+     end
+  end
+  private :planning_wizard_get_vms_records
+
+  def planning_wizard_get_vms(filter_type, filter_value)
+    vms = planning_wizard_get_vms_records(filter_type, filter_value)
+    vms.each_with_object({}) { |v, h| h[v.id.to_s] = v.name }
+  end
+  private :planning_wizard_get_vms
+
   def planning_option_changed
-    vms = nil
-    filter_value = params[:filter_value]
-    if filter_value && filter_value != "<Choose>"
-      case params[:filter_typ]
-      when "host"
-        vms = Host.find(filter_value).vms
-      when "ems"
-        vms = ExtManagementSystem.find(filter_value).vms
-      when "cluster"
-        vms = EmsCluster.find(filter_value).all_vms
-      when "filter"
-        vms = MiqSearch.find(filter_value).results(:userid => current_userid)
-      else
-        vms = []
-      end
-    elsif params[:filter_typ] == "all"
-      vms = find_filtered(Vm).sort_by { |v| v.name.downcase }
+    if params[:filter_typ].present?
+      filter_typ = params[:filter_typ] == "<Choose>" ? nil : params[:filter_typ]
+
+      @sb[:planning][:options][:filter_typ] = filter_typ
+      @sb[:planning][:vms] = planning_wizard_get_vms('all', nil) if filter_typ == 'all'
     end
-    @sb[:planning][:vms] = vms ? vms.each_with_object({}) { |v, h| h[v.id.to_s] = v.name } : nil
-    @sb[:planning][:options][:filter_typ] = params[:filter_typ] == "<Choose>" ? nil : params[:filter_typ]
-    @sb[:planning][:options][:filter_value] = params[:filter_value] == "<Choose>" ? nil : params[:filter_value]
-    @sb[:planning][:options][:chosen_vm] = params[:chosen_vm] == "<Choose>" ? nil : params[:chosen_vm]
+
+    if params[:filter_value].present?
+      filter_value = params[:filter_value] == "<Choose>" ? nil : params[:filter_value]
+
+      @sb[:planning][:options][:filter_value] = filter_value
+      @sb[:planning][:vms] = planning_wizard_get_vms(@sb[:planning][:options][:filter_typ], filter_value)
+    end
+
+    @sb[:planning][:options][:chosen_vm] = params[:chosen_vm] == "<Choose>" ? nil : params[:chosen_vm] if params[:chosen_vm]
     @sb[:planning][:options][:days] = params[:trend_days].to_i if params[:trend_days]
     @sb[:planning][:options][:vm_mode] = VALID_PLANNING_VM_MODES[params[:vm_mode]] if params[:vm_mode]
     @sb[:planning][:options][:trend_cpu] = (params[:trend_cpu] == "1") if params[:trend_cpu]