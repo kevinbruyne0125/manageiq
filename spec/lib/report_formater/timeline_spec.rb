kevinbruyne0125/manageiq

commit 4f650a999c893b7519d9e7648acea92b027f59ff

Refactor: Set instance attribute only once.

lib/report_formatter/timeline.rb
@@ -53,7 +53,6 @@ module ReportFormatter
     end
 
     def tl_event(tl_xml, row, col)
-      tl_message = TimelineMessage.new
       mri = options.mri
       tz = mri.get_time_zone(Time.zone.name)
       etime = row[col]
@@ -209,12 +208,13 @@ module ReportFormatter
       headers.delete(j)
       headers.push(j)
 
+      flags = {:ems_cloud     => ems_cloud,
+               :ems_container => ems_container,
+               :time_zone     => tz}
+      tl_message = TimelineMessage.new(row, rec, flags, mri.db)
       e_text = ''
       col_order.each_with_index do |co, co_idx|
-        flags = {:ems_cloud     => ems_cloud,
-                 :ems_container => ems_container,
-                 :time_zone     => tz}
-        val = tl_message.message_html(co, row, rec, flags, mri.db)
+        val = tl_message.message_html(co)
         e_text += "<b>#{headers[co_idx]}:</b> #{val}<br/>" unless val.to_s.empty? || co == "id"
       end
       e_text = e_text.chomp('<br/>')

lib/report_formatter/timeline_message.rb
@@ -2,6 +2,11 @@ module ReportFormatter
   class TimelineMessage
     include CompressedIds
 
+    def initialize(row, event, flags, db)
+      @row, @event, @flags, @db = row, event, flags, db
+      @ems_cloud, @ems_container = flags[:ems_cloud], flags[:ems_container]
+    end
+
     def vm_name
       "<a href=\"/vm/show/#{to_cid(@event.vm_or_template_id)}\">#{text}</a>" if @event.vm_or_template_id
     end
@@ -77,17 +82,8 @@ module ReportFormatter
       end
     end
 
-    def set_parameters(column, row, event, flags, db)
-      @event, @ems_cloud, @ems_container = event, flags[:ems_cloud], flags[:ems_container]
-      @db   = db
+    def message_html(column)
       @column = column
-      @row = row
-      @flags = flags
-    end
-
-    def message_html(column, row, event, flags, db)
-      set_parameters(column, row, event, flags, db)
-
       field = column.tr('.', '_').to_sym
       respond_to?(field) ? send(field).to_s : text
     end

spec/lib/report_formater/timeline_spec.rb
@@ -54,7 +54,7 @@ describe ReportFormatter::TimelineMessage do
     tests.each do |column, href|
       it "Evaluate column #{column} content" do
         row[column] = 'test timeline'
-        val = ReportFormatter::TimelineMessage.new.message_html(column, row, event, flags, 'EmsEvent')
+        val = ReportFormatter::TimelineMessage.new(row, event, flags, 'EmsEvent').message_html(column)
         expect(val).to eq(href)
       end
     end
@@ -79,7 +79,7 @@ describe ReportFormatter::TimelineMessage do
     tests.each do |column, href|
       it "Evaluate column #{column} content" do
         row[column] = 'test timeline'
-        val = ReportFormatter::TimelineMessage.new.message_html(column, row, event, flags, 'EmsEvent')
+        val = ReportFormatter::TimelineMessage.new(row, event, flags, 'EmsEvent').message_html(column)
         expect(val).to eq(href)
       end
     end
@@ -102,7 +102,7 @@ describe ReportFormatter::TimelineMessage do
     tests.each do |column, href|
       it "Evaluate column #{column} content" do
         row[column] = 'MemoryUsage'
-        val = ReportFormatter::TimelineMessage.new.message_html(column, row, event, {}, 'BottleneckEvent')
+        val = ReportFormatter::TimelineMessage.new(row, event, {}, 'BottleneckEvent').message_html(column)
         expect(val).to eq(href)
       end
     end

commit 9405b3c182afea910d2156e7cfaf59314d2bf2a0

Added missing target information and links on Policy Event bubbles.
changes in 4f650a999c893b7519d9e7648acea92b027f59ff introduced the issue variable e_text was initialized to '' ignoring the fact that it was being set in several places above in the code. Fixed code to use TimelineMessage class to set links and other target related information for PolicyEvent timelines event bubble.

https://bugzilla.redhat.com/show_bug.cgi?id=1402786

lib/report_formatter/timeline.rb
@@ -103,12 +103,10 @@ module ReportFormatter
           e_title = rec[:name]
           e_icon = ActionController::Base.helpers.image_path("timeline/vendor-#{rec.vendor.downcase}.png")
           e_image = ActionController::Base.helpers.image_path("100/os-#{rec.os_image_name.downcase}.png")
-          e_text = "&lt;a href=/vm/show/#{rec.id}&gt;#{e_title}&lt;/a&gt;"
         when "Host"
           e_title = rec[:name]
           e_icon = ActionController::Base.helpers.image_path("timeline/vendor-#{rec.vmm_vendor_display.downcase}.png")
           e_image = ActionController::Base.helpers.image_path("100/os-#{rec.os_image_name.downcase}.png")
-          e_text = "&lt;a href=/host/show/#{rec.id}&gt;#{e_title}&lt;/a&gt;"
         when "EventStream"
           ems_cloud = false
           if rec[:ems_id] && ExtManagementSystem.exists?(rec[:ems_id])
@@ -149,43 +147,9 @@ module ReportFormatter
           if rec[:vm_or_template_id] && Vm.exists?(rec[:vm_or_template_id])
             e_image = ActionController::Base.helpers.image_path("100/os-#{Vm.find(rec[:vm_or_template_id]).os_image_name.downcase}.png")
           end
-          e_text = e_title
-        when "PolicyEvent"
-          if rec[:target_name]              # Create the title using Policy description
-            e_title = rec[:target_name]
-          elsif rec[:miq_policy_id] && MiqPolicy.exists?(rec[:miq_policy_id])   #   or Policy name
-            e_title = MiqPolicy.find(rec[:miq_policy_id]).name
-          else
-            e_title = "Policy no longer exists"
-          end
-          e_title ||= "No Policy"
-          e_icon = ActionController::Base.helpers.image_path("100/event-#{rec.event_type.downcase}.png")
-          # e_icon = "/images/100/vendor-ec2.png"
-          e_text = e_title
-          unless rec.target_id.nil?
-            e_text += "<br/>&lt;a href=/#{Dictionary.gettext(rec.target_class, :type => :model, :notfound => :titleize).downcase}/show/#{to_cid(rec.target_id)}&gt;<b> #{Dictionary.gettext(rec.target_class, :type => :model, :notfound => :titleize)}:</b> #{rec.target_name}&lt;/a&gt;"
-          end
-
-          assigned_profiles = {}
-          profile_sets = rec.miq_policy_sets
-
-          profile_sets.each do |profile|
-            assigned_profiles[profile.id] = profile.description unless profile.description.nil?
-          end
-
-          unless rec.event_type.nil?
-            e_text += "<br/><b>Assigned Profiles:</b> "
-            assigned_profiles.each_with_index do |p, i|
-              e_text += "&lt;a href=/miq_policy/explorer?profile=#{p[0]}&gt;<b> #{p[1]}&lt;/a&gt;"
-              if assigned_profiles.length > 1 && i < assigned_profiles.length
-                e_text += ", "
-              end
-            end
-          end
         else
           e_title = rec[:name] ? rec[:name] : row[mri.col_order.first].to_s
           e_icon = image = nil
-          e_text = e_title
         end
       end
 

lib/report_formatter/timeline_message.rb
@@ -34,6 +34,32 @@ module ReportFormatter
       "<a href=/host/show/#{to_cid(@event.dest_host_id)}>#{text}</a>" if @event.dest_host_id
     end
 
+    def target_name
+      e_text = if @event.target_name # Create the title using Policy description
+                 @event.target_name
+               elsif @event.miq_policy_id && MiqPolicy.exists?(@event.miq_policy_id) # or Policy name
+                 MiqPolicy.find(@event.miq_policy_id).name
+               else
+                 _("Policy no longer exists")
+               end
+      unless @event.target_id.nil?
+        e_text += "<br><b>#{Dictionary.gettext(@event.target_class, :type => :model, :notfound => :titleize)}:</b>&nbsp;"
+        e_text += "<a href=/#{@event.target_class.underscore}/show/#{to_cid(@event.target_id)}>#{@event.target_name}</a>"
+      end
+      assigned_profiles = @event.miq_policy_sets.each_with_object({}) do |profile, hsh|
+        hsh[profile.id] = profile.description unless profile.description.nil?
+      end
+
+      unless @event.event_type.nil?
+        e_text += "<br/><b>#{_("Assigned Profiles")}:</b>&nbsp;"
+        assigned_profiles.each_with_index do |p, i|
+          e_text += "<a href=/miq_policy/explorer/?id=pp-#{p[0]}>#{p[1]}</a>"
+          e_text += ", " if assigned_profiles.length > 1 && i < assigned_profiles.length
+        end
+      end
+      e_text
+    end
+
     def ems_cluster_name
       "<a href=/ems_cluster/show/#{to_cid(@event.ems_cluster_id)}>#{text}</a>" if @event.ems_cluster_id
     end

product/timelines/miq_reports/tl_policy_events_daily.yaml
@@ -24,6 +24,9 @@ cols:
 - miq_event_description
 - miq_policy_description
 - result
+- target_name
+- target_id
+- target_class
 
 # Included tables (joined, has_one, has_many) and columns
 include:
@@ -35,6 +38,7 @@ col_order:
 - miq_event_description
 - miq_policy_description
 - result
+- target_name
 
 # Column titles, in order
 headers:
@@ -43,6 +47,7 @@ headers:
 - Miq Event Description
 - Miq Policy Description
 - Result
+- Tagret
 
 # Condition(s) string for the SQL query
 conditions:

product/timelines/miq_reports/tl_policy_events_hourly.yaml
@@ -24,6 +24,9 @@ cols:
 - miq_event_description
 - miq_policy_description
 - result
+- target_name
+- target_id
+- target_class
 
 # Included tables (joined, has_one, has_many) and columns
 include:
@@ -35,6 +38,7 @@ col_order:
 - miq_event_description
 - miq_policy_description
 - result
+- target_name
 
 # Column titles, in order
 headers:
@@ -43,6 +47,7 @@ headers:
 - Miq Event Description
 - Miq Policy Description
 - Result
+- Target
 
 # Condition(s) string for the SQL query
 conditions:

spec/lib/report_formater/timeline_spec.rb
@@ -106,6 +106,29 @@ describe ReportFormatter::TimelineMessage do
     end
   end
 
+  describe '#message_html on policy event' do
+    row = {}
+    let(:vm) { FactoryGirl.create(:vm_redhat, :id => 42, :name => 'Test VM') }
+    let(:event) do
+      FactoryGirl.create(:policy_event,
+                         :event_type   => 'vm_poweroff',
+                         :target_id    => 42,
+                         :target_name  => vm.name,
+                         :target_class => 'VmOrTemplate')
+    end
+
+    tests = {'event_type'  => 'vm_poweroff',
+             'target_name' => 'Test VM<br><b>VM or Template:</b>&nbsp;<a href=/vm_or_template/show/42>Test VM</a><br/><b>Assigned Profiles:</b>&nbsp;'}
+
+    tests.each do |column, href|
+      it "Evaluate column #{column} content" do
+        row[column] = 'vm_poweroff'
+        val = ReportFormatter::TimelineMessage.new(row, event, {}, 'PolicyEvent').message_html(column)
+        expect(val).to eq(href)
+      end
+    end
+  end
+
   describe '#events count for different categories' do
     def stub_ems_event(event_type)
       ems = FactoryGirl.create(:ems_redhat)