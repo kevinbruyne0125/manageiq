kevinbruyne0125/manageiq

commit ea71252ec95cc834404986ed5e51cf8b84ae5fa8

Moved conversion of event to event_hash outside of EmsEvent so as to remove provider specific add_* methods

app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
@@ -50,7 +50,8 @@ module ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin
   def queue_event(event)
     event_data = extract_event_data(event)
     _log.info "#{log_prefix} Queuing event [#{event_data}]"
-    EmsEvent.add_queue('add_kubernetes', @cfg[:ems_id], event_data)
+    event_hash = ManageIQ::Providers::Kubernetes::ContainerManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 
   def filtered?(event)

app/models/manageiq/providers/google/cloud_manager/event_catcher/runner.rb
@@ -25,7 +25,8 @@ class ManageIQ::Providers::Google::CloudManager::EventCatcher::Runner <
 
   def queue_event(event)
     _log.info "#{log_prefix} Caught event #{parse_event_type(event)} for #{parse_resource_id(event)}"
-    EmsEvent.add_queue('add_google', @cfg[:ems_id], event)
+    event_hash = ManageIQ::Providers::Google::CloudManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 
   private

app/models/manageiq/providers/openstack/cloud_manager/event_catcher/runner.rb
@@ -1,7 +1,8 @@
 class ManageIQ::Providers::Openstack::CloudManager::EventCatcher::Runner < ManageIQ::Providers::BaseManager::EventCatcher::Runner
   include ManageIQ::Providers::Openstack::EventCatcherMixin
 
-  def add_openstack_queue(event_hash)
-    EmsEvent.add_queue('add_openstack', @cfg[:ems_id], event_hash)
+  def add_openstack_queue(event)
+    event_hash = ManageIQ::Providers::Openstack::CloudManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 end

app/models/manageiq/providers/openstack/infra_manager/event_catcher/runner.rb
@@ -1,7 +1,8 @@
 class ManageIQ::Providers::Openstack::InfraManager::EventCatcher::Runner < ManageIQ::Providers::BaseManager::EventCatcher::Runner
   include ManageIQ::Providers::Openstack::EventCatcherMixin
 
-  def add_openstack_queue(event_hash)
-    EmsEvent.add_queue('add_openstack_infra', @cfg[:ems_id], event_hash)
+  def add_openstack_queue(event)
+    event_hash = ManageIQ::Providers::Openstack::InfraManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 end

app/models/manageiq/providers/openstack/network_manager/event_catcher/runner.rb
@@ -1,7 +1,8 @@
 class ManageIQ::Providers::Openstack::NetworkManager::EventCatcher::Runner < ManageIQ::Providers::BaseManager::EventCatcher::Runner
   include ManageIQ::Providers::Openstack::EventCatcherMixin
 
-  def add_openstack_queue(event_hash)
-    EmsEvent.add_queue('add_openstack_network', @cfg[:ems_id], event_hash)
+  def add_openstack_queue(event)
+    event_hash = ManageIQ::Providers::Openstack::NetworkManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 end

app/models/manageiq/providers/redhat/infra_manager/event_catcher/runner.rb
@@ -41,7 +41,8 @@ class ManageIQ::Providers::Redhat::InfraManager::EventCatcher::Runner < ManageIQ
 
   def queue_event(event)
     _log.info "#{log_prefix} Caught event [#{event[:name]}]"
-    EmsEvent.add_queue('add_rhevm', @cfg[:ems_id], event.to_hash)
+    event_hash = ManageIQ::Providers::Redhat::InfraManager::EventParser.event_to_hash(event, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 
   def filtered?(event)

app/models/manageiq/providers/storage_manager/cinder_manager/event_catcher/runner.rb
@@ -1,6 +1,7 @@
 class ManageIQ::Providers::StorageManager::CinderManager::EventCatcher::Runner <
   ManageIQ::Providers::BaseManager::EventCatcher::Runner
   def add_cinder_queue(event_hash)
-    EmsEvent.add_queue('add_cinder', @cfg[:ems_id], event_hash)
+    event_hash = ManageIQ::Providers::StorageManager::CinderManager::EventParser.event_to_hash(event_hash, :ems_id)
+    EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 end

commit a2af3104ea5fc422a0ddc5fec5bd90a6fb623a32

Fix k8s event collection
First broken in ea71252 and partially fixed in a490a25

app/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin.rb
@@ -50,7 +50,7 @@ module ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin
   def queue_event(event)
     event_data = extract_event_data(event)
     _log.info "#{log_prefix} Queuing event [#{event_data}]"
-    event_hash = ManageIQ::Providers::Kubernetes::ContainerManager::EventParser.event_to_hash(event, @cfg[:ems_id])
+    event_hash = ManageIQ::Providers::Kubernetes::ContainerManager::EventParser.event_to_hash(event_data, @cfg[:ems_id])
     EmsEvent.add_queue('add', @cfg[:ems_id], event_hash)
   end
 

spec/models/manageiq/providers/kubernetes/container_manager/event_catcher_mixin_spec.rb
@@ -8,6 +8,23 @@ describe ManageIQ::Providers::Kubernetes::ContainerManager::EventCatcherMixin do
   end
   let(:ems) { FactoryGirl.create(:ems_kubernetes) }
 
+  describe '#queue_event' do
+    it 'sends the received event to queue after parsing' do
+      catcher = test_class.new(ems)
+      expect(catcher).to receive(:extract_event_data).once.with(:raw_event).and_return(:extracted_event)
+      expect(catcher).to receive(:log_prefix)
+      expect(catcher).to receive_message_chain("_log.info")
+      catcher.instance_variable_set(:@cfg, :ems_id => ems.id)
+      expect(
+        ManageIQ::Providers::Kubernetes::ContainerManager::EventParser
+      ).to receive(
+        :event_to_hash
+      ).with(:extracted_event, ems.id).and_return(:hashed_event)
+      expect(EmsEvent).to receive(:add_queue).with('add', ems.id, :hashed_event)
+      catcher.queue_event(:raw_event)
+    end
+  end
+
   describe '#extract_event_data' do
     context 'given container event (Pod event with fieldPath)' do
       let(:kubernetes_event) do