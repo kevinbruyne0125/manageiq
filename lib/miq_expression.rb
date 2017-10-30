kevinbruyne0125/manageiq

commit 931467201828d4a17699765c2ac38907244084fe

Let callers decide whether to call tag_details

lib/miq_expression.rb
@@ -801,7 +801,11 @@ class MiqExpression
 
     opts = {:typ => "all", :include_model => true}.merge(opts)
     if opts[:typ] == "tag"
-      tags_for_model = tag_details(model, model, opts)
+      tags_for_model = if TAG_CLASSES.include?(model)
+                         tag_details(model, model, opts)
+                       else
+                         []
+                       end
       result = []
       TAG_CLASSES.invert.each do |name, tc|
         next if tc.constantize.base_class == model.constantize.base_class
@@ -825,8 +829,7 @@ class MiqExpression
         custom_details = _custom_details_for(model, opts)
         result.concat(custom_details.sort_by(&:to_s)) unless custom_details.empty?
       end
-
-      result.concat(tag_details(model, model, opts)) if opts[:include_tags] == true
+      result.concat(tag_details(model, model, opts)) if opts[:include_tags] == true && TAG_CLASSES.include?(model)
     end
 
     model_details = _model_details(relats, opts)
@@ -869,7 +872,9 @@ class MiqExpression
         result.concat(get_column_details(ref[:columns], parent[:class_path], parent[:assoc_path], opts)) if parent[:multivalue]
       else
         result.concat(get_column_details(ref[:columns], parent[:class_path], parent[:assoc_path], opts))
-        result.concat(tag_details(parent[:assoc_class], parent[:class_path], opts)) if opts[:include_tags] == true
+        if opts[:include_tags] == true && TAG_CLASSES.include?(parent[:assoc_class])
+          result.concat(tag_details(parent[:assoc_class], parent[:class_path], opts))
+        end
       end
 
       result.concat(_model_details(ref, opts))
@@ -878,7 +883,6 @@ class MiqExpression
   end
 
   def self.tag_details(model, path, opts)
-    return [] unless TAG_CLASSES.include?(model)
     result = []
     @classifications ||= categories
     @classifications.each do |name, cat|
@@ -922,7 +926,11 @@ class MiqExpression
       md = model_details(model, :include_model => false, :include_tags => true).select do |c|
         c.last.ends_with?(*ReportController::Reports::Editor::CHARGEBACK_ALLOWED_FIELD_SUFFIXES)
       end
-      td = tag_details(cb_model, model, {}) + _custom_details_for(cb_model, {})
+      td = if TAG_CLASSES.include?(cb_model)
+             tag_details(cb_model, model, {}) + _custom_details_for(cb_model, {})
+           else
+             []
+           end
       md + td
     else
       model_details(model, :include_model => false, :include_tags => true)

commit e1c21b128844c4fddea2249a6603b5565c0ac5a9

Add back list of custom attributes
accidently caused by:
https://github.com/ManageIQ/manageiq/pull/15788/commits/931467201828d4a17699765c2ac38907244084fe#diff-e5855dfa8cc9aeeeb9d13acd1525499eR929

lib/miq_expression.rb
@@ -931,11 +931,11 @@ class MiqExpression
         c.last.ends_with?(*allowed_suffixes)
       end
       td = if TAG_CLASSES.include?(cb_model)
-             tag_details(model, {}) + _custom_details_for(cb_model, {})
+             tag_details(model, {})
            else
              []
            end
-      md + td
+      md + td + _custom_details_for(cb_model, {})
     else
       model_details(model, :include_model => false, :include_tags => true)
     end