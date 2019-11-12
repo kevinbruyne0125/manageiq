kevinbruyne0125/manageiq

commit fefccb69bc27b4a9f0cd3d99d1e9cf20c10316ea

Scale to 0 when requred role is missing
Previously MiqWorker.workers would return the configured count
even if the required roles for the worker type were not met.

app/models/miq_worker.rb
@@ -48,6 +48,7 @@ class MiqWorker < ApplicationRecord
 
   def self.workers
     return (self.has_minimal_env_option? ? 1 : 0) if MiqServer.minimal_env? && check_for_minimal_role
+    return 0 unless has_required_role?
     return @workers.call if @workers.kind_of?(Proc)
     return @workers unless @workers.nil?
     workers_configured_count
@@ -137,7 +138,7 @@ class MiqWorker < ApplicationRecord
   def self.sync_workers
     w       = include_stopping_workers_on_synchronize ? find_alive : find_current_or_starting
     current = w.length
-    desired = self.has_required_role? ? workers : 0
+    desired = workers
     result  = {:adds => [], :deletes => []}
 
     if current != desired

app/models/miq_worker/container_common.rb
@@ -14,8 +14,8 @@ class MiqWorker
     end
 
     def scale_deployment
-      ContainerOrchestrator.new.scale(worker_deployment_name, self.class.workers_configured_count)
-      delete_container_objects if self.class.workers_configured_count.zero?
+      ContainerOrchestrator.new.scale(worker_deployment_name, self.class.workers)
+      delete_container_objects if self.class.workers.zero?
     end
 
     def container_image_namespace

app/models/mixins/miq_web_server_worker_mixin.rb
@@ -55,7 +55,7 @@ module MiqWebServerWorkerMixin
 
       workers = find_current_or_starting
       current = workers.length
-      desired = self.has_required_role? ? self.workers : 0
+      desired = self.workers
       result  = {:adds => [], :deletes => []}
       ports = all_ports_in_use
 

commit 2c04cb2439bb3d6c374958aeeff53d135601034e

Use the workers proc rather than overriding the method
Commit fefccb69bc27b4a9f0cd3d99d1e9cf20c10316ea caused us to start
metrics collector workers even if the role was not active.

This fixes that issue and adds tests to the `.workers` method for
the PerEmsTypeWorkerMixin

app/models/mixins/per_ems_type_worker_mixin.rb
@@ -3,15 +3,13 @@ module PerEmsTypeWorkerMixin
 
   included do
     self.check_for_minimal_role = false
+    @workers = lambda do
+      return 0 unless self.any_valid_ems_in_zone?
+      workers_configured_count
+    end
   end
 
   module ClassMethods
-    def workers
-      return 0 unless self.any_valid_ems_in_zone?
-      return (self.has_minimal_env_option? ? 1 : 0) if MiqServer.minimal_env?
-      workers_configured_count
-    end
-
     def ems_class
       ExtManagementSystem
     end

spec/factories/miq_worker.rb
@@ -11,6 +11,10 @@ FactoryBot.define do
 
   factory :miq_ems_metrics_processor_worker, :class => "MiqEmsMetricsProcessorWorker", :parent => :miq_worker
 
+  factory :miq_ems_metrics_collector_worker,
+          :class  => "ManageIQ::Providers::BaseManager::MetricsCollectorWorker",
+          :parent => :miq_worker
+
   factory :miq_ems_refresh_worker,
           :parent => :miq_worker,
           :class  => "ManageIQ::Providers::BaseManager::RefreshWorker"

spec/models/mixins/per_ems_type_worker_mixin_spec.rb
@@ -1,0 +1,46 @@
+describe PerEmsTypeWorkerMixin do
+  let(:worker)       { FactoryBot.create(:miq_ems_metrics_collector_worker) }
+  let(:worker_class) { worker.class }
+
+  before do
+    _guid, @server, @zone = EvmSpecHelper.create_guid_miq_server_zone
+  end
+
+  describe ".workers" do
+    it "is 0 with no providers" do
+      expect(worker_class.workers).to eq(0)
+    end
+
+    context "with a provider" do
+      let!(:provider) { FactoryBot.create(:ems_vmware, :with_authentication, :zone => @zone) }
+
+      it "is 0 without the role active" do
+        expect(worker_class.workers).to eq(0)
+      end
+
+      context "with the role active" do
+        before do
+          ServerRole.seed
+          @server.role = "ems_metrics_collector"
+          @server.assigned_server_roles.update(:active => true)
+        end
+
+        it "is the number configured" do
+          configured = worker_class.workers_configured_count
+          expect(worker_class.workers).to eq(configured)
+
+          stub_settings(:workers => {:worker_base => {:queue_worker_base => {:ems_metrics_collector_worker => {:count => configured + 1}}}})
+          expect(worker_class.workers).to eq(configured + 1)
+        end
+
+        it "is 0 with the provider in the wrong zone" do
+          other_zone = FactoryBot.create(:zone)
+          provider.update(:zone => other_zone)
+          stub_settings(:workers => {:worker_base => {:queue_worker_base => {:ems_metrics_collector_worker => {:count => 5}}}})
+
+          expect(worker_class.workers).to eq(0)
+        end
+      end
+    end
+  end
+end